39f926e18154156e8c405426383dfcb9c5b83327
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2020-12-12 18:46:38 +0100

    Fix Pipeline

---
+++ src/test/java/tech/bugger/BuggerTest.java
@@ -0,0 +1,12 @@
+package tech.bugger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BuggerTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
