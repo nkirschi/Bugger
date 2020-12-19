7e0ae9adcfcd1df6e4bb8d34ff41ed623ef29bfb
Nico Mexis <nico.mexis@kabelmail.de>
2020-12-19 14:54:30 +0100

    Added basic Markdown parsing

--- src/main/java/tech/bugger/business/util/MarkdownHandler.java
+++ src/main/java/tech/bugger/business/util/MarkdownHandler.java
@@ -1,5 +1,10 @@
 package tech.bugger.business.util;
 
+import java.util.Collections;
+import java.util.List;
+import org.commonmark.Extension;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import tech.bugger.global.util.Log;
 
 /**
@@ -7,7 +12,25 @@ import tech.bugger.global.util.Log;
  */
 public final class MarkdownHandler {
 
+    /**
+     * The {@link Log} to log errors, warnings and other debug information to.
+     */
     private static final Log log = Log.forClass(MarkdownHandler.class);
+
+    /**
+     * All {@link Extension}s to load for the commonmark-java library.
+     */
+    private static final List<Extension> EXTENSIONS = Collections.singletonList(ReferenceExtension.create());
+
+    /**
+     * The {@link Parser} used to parse Markdown via the commonmark-java library.
+     */
+    private static final Parser PARSER = Parser.builder().extensions(EXTENSIONS).build();
+
+    /**
+     * The {@link HtmlRenderer} used to render HTML from Markdown via the commonmark-java library.
+     */
+    private static final HtmlRenderer RENDERER = HtmlRenderer.builder().escapeHtml(true).extensions(EXTENSIONS).build();
 
     private MarkdownHandler() {
     }
@@ -18,8 +41,11 @@ public final class MarkdownHandler {
      * @param md The input formatted as Markdown.
      * @return The parsed and rendered equivalent HTML Output.
      */
-    public static String toHtml(String md) {
-        return null;
+    public static String toHtml(final String md) {
+        if (md == null) {
+            return "";
+        }
+        return RENDERER.render(PARSER.parse(md));
     }
 
 }
