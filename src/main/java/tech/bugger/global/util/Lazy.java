35c943a4b64b9e13bd99e85441c67b99249d70cc
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2020-12-20 22:21:32 +0100

    Implement Laziness

--- src/main/java/tech/bugger/global/util/Lazy.java
+++ src/main/java/tech/bugger/global/util/Lazy.java
@@ -27,6 +27,9 @@ public class Lazy<T> implements Serializable {
      * @param retrieval The instructions for acquiring the wrapped object.
      */
     public Lazy(Supplier<T> retrieval) {
+        if (retrieval == null) {
+            throw new IllegalArgumentException("Retrieval function must not be null!");
+        }
         this.retrieval = retrieval;
     }
 
@@ -36,6 +39,9 @@ public class Lazy<T> implements Serializable {
      * @param value The object to be immediately wrapped.
      */
     public Lazy(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Wrapped value must not be null!");
+        }
         this.value = value;
     }
 
---
+++ src/test/java/tech/bugger/global/util/LazyTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020. SEParatisten
+ */
+
+package tech.bugger.global.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LazyTest {
+    private Lazy<String> lazyString;
+    private Lazy<String> eagerString;
+
+    @BeforeEach
+    public void init() {
+        lazyString = new Lazy<>(() -> "lazy");
+        eagerString = new Lazy<>("eager");
+    }
+
+    @Test
+    public void testLazyConsructorWhenNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Lazy<>((Supplier<String>) null));
+    }
+
+    @Test
+    public void testEagerConsructorWhenNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Lazy<>((String) null));
+    }
+
+    @Test
+    public void testIsPresentWhenNotPresent() {
+        assertFalse(lazyString.isPresent());
+    }
+
+    @Test
+    public void testIsPresentWhenPresent() {
+        lazyString.get();
+        assertTrue(lazyString.isPresent());
+    }
+
+    @Test
+    public void testIsPresentOnEager() {
+        assertTrue(eagerString.isPresent());
+    }
+
+    @Test
+    public void testGetOnLazy() {
+        assertEquals("lazy", lazyString.get());
+    }
+
+    @Test
+    public void testGetOnEager() {
+        assertEquals("eager", eagerString.get());
+    }
+}
