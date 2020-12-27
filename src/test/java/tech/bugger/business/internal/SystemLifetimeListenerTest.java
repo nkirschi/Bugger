736316d84c3b53cd43c6f6f7be3604faa57bb121
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2020-12-27 12:03:13 +0100

    Fix Missing Mock in Tests

--- src/test/java/tech/bugger/business/internal/SystemLifetimeListenerTest.java
+++ src/test/java/tech/bugger/business/internal/SystemLifetimeListenerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
+import tech.bugger.business.util.PriorityExecutor;
+import tech.bugger.business.util.PriorityExecutorRegistry;
 import tech.bugger.global.util.Log;
 import tech.bugger.persistence.exception.TransactionException;
 import tech.bugger.persistence.gateway.MetadataGateway;
@@ -24,8 +26,6 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -38,18 +38,24 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class SystemLifetimeListenerTest {
-    private SystemLifetimeListener systemLifetimeListener;
+    private SystemLifetimeListener systemLifetimeListenerMock;
 
-    private MockedStatic<Log> logMock;
-    private PropertiesReaderRegistry propertiesReaderRegistry;
-    private ConnectionPoolRegistry connectionPoolRegistry;
-    private MailerRegistry mailerRegistry;
-    private TransactionManager transactionManager;
-    private Transaction tx;
-    private MetadataGateway mg;
+    private MockedStatic<Log> logStaticMock;
+    private MockedStatic<Runtime> runtimeStaticMock;
 
-    private ServletContextEvent sce;
-    private ServletContext sctx;
+    private PropertiesReaderRegistry propertiesReaderRegistryMock;
+    private ConnectionPoolRegistry connectionPoolRegistryMock;
+    private MailerRegistry mailerRegistryMock;
+    private PriorityExecutorRegistry priorityExecutorRegistryMock;
+    private TransactionManager transactionManagerMock;
+    private ConnectionPool connectionPoolMock;
+    private PriorityExecutor priorityExecutorMock;
+    private Transaction transactionMock;
+    private MetadataGateway metadataGatewayMock;
+    private Runtime runtimeMock;
+
+    private ServletContextEvent sceMock;
+    private ServletContext sctxMock;
 
     private static EmbeddedPostgres pg;
 
@@ -65,7 +71,7 @@ public class SystemLifetimeListenerTest {
 
     @BeforeEach
     public void setUp() {
-        logMock = mockStatic(Log.class);
+        logStaticMock = mockStatic(Log.class);
         Log log = mock(Log.class);
         doNothing().when(log).debug(any());
         doNothing().when(log).info(any());
@@ -75,18 +81,20 @@ public class SystemLifetimeListenerTest {
         doNothing().when(log).info(any(), any());
         doNothing().when(log).warning(any(), any());
         doNothing().when(log).error(any(), any());
-        logMock.when(() -> Log.forClass(any())).thenReturn(log);
+        logStaticMock.when(() -> Log.forClass(any())).thenReturn(log);
 
-        propertiesReaderRegistry = mock(PropertiesReaderRegistry.class);
-        connectionPoolRegistry = mock(ConnectionPoolRegistry.class);
-        mailerRegistry = mock(MailerRegistry.class);
-        transactionManager = mock(TransactionManager.class);
+        propertiesReaderRegistryMock = mock(PropertiesReaderRegistry.class);
+        connectionPoolRegistryMock = mock(ConnectionPoolRegistry.class);
+        mailerRegistryMock = mock(MailerRegistry.class);
+        transactionManagerMock = mock(TransactionManager.class);
+        priorityExecutorRegistryMock = mock(PriorityExecutorRegistry.class);
 
-        systemLifetimeListener = new SystemLifetimeListener();
-        systemLifetimeListener.setPropertiesReaderRegistry(propertiesReaderRegistry);
-        systemLifetimeListener.setConnectionPoolRegistry(connectionPoolRegistry);
-        systemLifetimeListener.setMailerRegistry(mailerRegistry);
-        systemLifetimeListener.setTransactionManager(transactionManager);
+        systemLifetimeListenerMock = new SystemLifetimeListener();
+        systemLifetimeListenerMock.setPropertiesReaderRegistry(propertiesReaderRegistryMock);
+        systemLifetimeListenerMock.setConnectionPoolRegistry(connectionPoolRegistryMock);
+        systemLifetimeListenerMock.setMailerRegistry(mailerRegistryMock);
+        systemLifetimeListenerMock.setTransactionManager(transactionManagerMock);
+        systemLifetimeListenerMock.setPriorityExecutorRegistry(priorityExecutorRegistryMock);
 
         PropertiesReader propertiesReader = mock(PropertiesReader.class);
         when(propertiesReader.getString(any())).thenReturn("");
@@ -94,145 +102,158 @@ public class SystemLifetimeListenerTest {
         when(propertiesReader.getString("DB_URL")).thenReturn("jdbc:postgresql://localhost:42424/postgres");
         when(propertiesReader.getInt(any())).thenReturn(1);
         when(propertiesReader.getBoolean(any())).thenReturn(false);
-        when(propertiesReaderRegistry.get(any())).thenReturn(propertiesReader);
+        when(propertiesReaderRegistryMock.get(any())).thenReturn(propertiesReader);
 
-        tx = mock(Transaction.class);
-        mg = mock(MetadataGateway.class);
-        when(mg.retrieveVersion()).thenReturn(null);
-        doNothing().when(mg).initializeSchema(any());
-        when(tx.newMetadataGateway()).thenReturn(mg);
-        when(transactionManager.begin()).thenReturn(tx);
+        connectionPoolMock = mock(ConnectionPool.class);
+        doNothing().when(connectionPoolMock).shutdown();
+        when(connectionPoolMock.isShutDown()).thenReturn(false);
+        when(connectionPoolRegistryMock.get(any())).thenReturn(connectionPoolMock);
 
+        priorityExecutorMock = mock(PriorityExecutor.class);
+        doNothing().when(priorityExecutorMock).shutdown();
+        when(priorityExecutorRegistryMock.get(any())).thenReturn(priorityExecutorMock);
 
-        sce = mock(ServletContextEvent.class);
-        sctx = mock(ServletContext.class);
-        when(sctx.getResourceAsStream(any())).thenReturn(new ByteArrayInputStream(new byte[0]));
-        when(sctx.getResourceAsStream("/WEB-INF/jdbc.properties"))
+        transactionMock = mock(Transaction.class);
+        metadataGatewayMock = mock(MetadataGateway.class);
+        when(metadataGatewayMock.retrieveVersion()).thenReturn(null);
+        when(transactionMock.newMetadataGateway()).thenReturn(metadataGatewayMock);
+        when(transactionManagerMock.begin()).thenReturn(transactionMock);
+
+        runtimeMock = mock(Runtime.class);
+        runtimeStaticMock = mockStatic(Runtime.class);
+        runtimeStaticMock.when(Runtime::getRuntime).thenReturn(runtimeMock);
+
+        sceMock = mock(ServletContextEvent.class);
+        sctxMock = mock(ServletContext.class);
+        when(sctxMock.getResourceAsStream(any())).thenReturn(new ByteArrayInputStream(new byte[0]));
+        when(sctxMock.getResourceAsStream("/WEB-INF/jdbc.properties"))
                 .thenReturn(ClassLoader.getSystemResourceAsStream("jdbc.properties"));
-        when(sce.getServletContext()).thenReturn(sctx);
+        when(sceMock.getServletContext()).thenReturn(sctxMock);
     }
 
     @AfterEach
     public void tearDown() {
-        logMock.close();
+        logStaticMock.close();
+        runtimeStaticMock.close();
     }
 
     @Test
     public void testContextInitializedInitializesLog() {
-        systemLifetimeListener.contextInitialized(sce);
-        logMock.verify(() -> Log.init(any()));
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        logStaticMock.verify(() -> Log.init(any()));
     }
 
     @Test
     public void testContextInitializedWhenLogInitFails() {
-        logMock.when(() -> Log.init(any())).thenThrow(IOException.class);
-        assertThrows(InternalError.class, () -> systemLifetimeListener.contextInitialized(sce));
+        logStaticMock.when(() -> Log.init(any())).thenThrow(IOException.class);
+        assertThrows(InternalError.class, () -> systemLifetimeListenerMock.contextInitialized(sceMock));
     }
 
     @Test
     public void testContextInitializedInitializesConfigReader() {
-        systemLifetimeListener.contextInitialized(sce);
-        verify(propertiesReaderRegistry).register(any(), any());
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(propertiesReaderRegistryMock).register(any(), any());
     }
 
     @Test
     public void testContextInitializedWhenConfigLoadingFails() throws IOException {
         InputStream badStream = new BufferedInputStream(new ByteArrayInputStream(new byte[0]));
         badStream.close();
-        when(sctx.getResourceAsStream("/WEB-INF/config.properties")).thenReturn(badStream);
-        assertThrows(InternalError.class, () -> systemLifetimeListener.contextInitialized(sce));
+        when(sctxMock.getResourceAsStream("/WEB-INF/config.properties")).thenReturn(badStream);
+        assertThrows(InternalError.class, () -> systemLifetimeListenerMock.contextInitialized(sceMock));
     }
 
     @Test
     public void testContextInitializedInitializesConnectionPool() {
-        systemLifetimeListener.contextInitialized(sce);
-        verify(connectionPoolRegistry).register(any(), any());
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(connectionPoolRegistryMock).register(any(), any());
     }
 
     @Test
     public void testContextInitializedWhenConnectionPoolInitFails() throws IOException {
         InputStream badStream = new BufferedInputStream(new ByteArrayInputStream(new byte[0]));
         badStream.close();
-        when(sctx.getResourceAsStream("/WEB-INF/jdbc.properties")).thenReturn(badStream);
-        assertThrows(InternalError.class, () -> systemLifetimeListener.contextInitialized(sce));
+        when(sctxMock.getResourceAsStream("/WEB-INF/jdbc.properties")).thenReturn(badStream);
+        assertThrows(InternalError.class, () -> systemLifetimeListenerMock.contextInitialized(sceMock));
     }
 
     @Test
     public void testContextInitializedInitializesDatabaseSchema() {
-        systemLifetimeListener.contextInitialized(sce);
-        verify(mg).initializeSchema(any());
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(metadataGatewayMock).initializeSchema(any());
     }
     
     @Test
     public void testContextInitializedWhenSchemaAlreadyPresent() {
-        when(mg.retrieveVersion()).thenReturn("1.0");
-        systemLifetimeListener.contextInitialized(sce);
-        verify(mg, times(0)).initializeSchema(any());
+        when(metadataGatewayMock.retrieveVersion()).thenReturn("1.0");
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(metadataGatewayMock, times(0)).initializeSchema(any());
     }
 
     @Test
     public void testContextInitializedWhenSchemaLoadingFails() {
-        when(sctx.getResourceAsStream("/WEB-INF/setup.sql")).thenReturn(null);
-        assertThrows(InternalError.class, () -> systemLifetimeListener.contextInitialized(sce));
+        when(sctxMock.getResourceAsStream("/WEB-INF/setup.sql")).thenReturn(null);
+        assertThrows(InternalError.class, () -> systemLifetimeListenerMock.contextInitialized(sceMock));
     }
 
     @Test
     public void testContextInitializedWhenSchemaTransactionError() throws Exception{
-        doThrow(TransactionException.class).when(tx).commit();
-        assertThrows(InternalError.class, () -> systemLifetimeListener.contextInitialized(sce));
+        doThrow(TransactionException.class).when(transactionMock).commit();
+        assertThrows(InternalError.class, () -> systemLifetimeListenerMock.contextInitialized(sceMock));
     }
 
     @Test
     public void testContextInitializedInitializesMailer() {
-        systemLifetimeListener.contextInitialized(sce);
-        verify(mailerRegistry).register(any(), any());
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(mailerRegistryMock).register(any(), any());
     }
 
     @Test
     public void testContextInitializedWhenMailerInitFails() throws IOException {
         InputStream badStream = new BufferedInputStream(new ByteArrayInputStream(new byte[0]));
         badStream.close();
-        when(sctx.getResourceAsStream("/WEB-INF/mailing.properties")).thenReturn(badStream);
-        assertThrows(InternalError.class, () -> systemLifetimeListener.contextInitialized(sce));
+        when(sctxMock.getResourceAsStream("/WEB-INF/mailing.properties")).thenReturn(badStream);
+        assertThrows(InternalError.class, () -> systemLifetimeListenerMock.contextInitialized(sceMock));
     }
 
     @Test
-    public void testContextInitializedAddsShutdownHook() throws Exception {
-        Runtime runtime = mock(Runtime.class);
-        MockedStatic<Runtime> runtimeMock = mockStatic(Runtime.class);
-        runtimeMock.when(Runtime::getRuntime).thenReturn(runtime);
-        doNothing().when(runtime).addShutdownHook(any());
-        systemLifetimeListener.contextInitialized(sce);
-        verify(runtime).addShutdownHook(any());
-        runtimeMock.close();
+    public void testContextInitializedInitializesPriorityExecutor() {
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(priorityExecutorRegistryMock).register(any(), any());
+    }
+
+    @Test
+    public void testContextInitializedAddsShutdownHooks() {
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        verify(runtimeMock, times(2)).addShutdownHook(any());
     }
 
     @Test
     public void testContextDestroyedShutsDownConnectionPool() {
-        ConnectionPool connectionPool = mock(ConnectionPool.class);
-        doNothing().when(connectionPool).shutdown();
-        when(connectionPool.isShutDown()).thenReturn(false);
-        when(connectionPoolRegistry.get(any())).thenReturn(connectionPool);
-        systemLifetimeListener.contextInitialized(sce);
-        systemLifetimeListener.contextDestroyed(sce);
-        verify(connectionPool).shutdown();
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        systemLifetimeListenerMock.contextDestroyed(sceMock);
+        verify(connectionPoolMock).shutdown();
     }
 
     @Test
     public void testContextDestroyedWhenConnectionPoolAlreadyShutDown() {
-        ConnectionPool connectionPool = mock(ConnectionPool.class);
-        doNothing().when(connectionPool).shutdown();
-        when(connectionPool.isShutDown()).thenReturn(true);
-        when(connectionPoolRegistry.get(any())).thenReturn(connectionPool);
-        systemLifetimeListener.contextInitialized(sce);
-        systemLifetimeListener.contextDestroyed(sce);
-        verify(connectionPool, times(0)).shutdown();
+        when(connectionPoolMock.isShutDown()).thenReturn(true);
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        systemLifetimeListenerMock.contextDestroyed(sceMock);
+        verify(connectionPoolMock, times(0)).shutdown();
     }
 
-    private Map<Thread, Thread> getShutdownHooks() throws Exception {
-        Class<?> clazz = Class.forName("java.lang.ApplicationShutdownHooks");
-        Field field = clazz.getDeclaredField("hooks");
-        field.setAccessible(true);
-        return (Map<Thread, Thread>) field.get(null);
+    @Test
+    public void testContextDestroyedShutsDownPriorityExecutor() {
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        systemLifetimeListenerMock.contextDestroyed(sceMock);
+        verify(priorityExecutorMock).shutdown();
+    }
+
+    @Test
+    public void testContextDestroyedDeregistersShutdownHooks() {
+        systemLifetimeListenerMock.contextInitialized(sceMock);
+        systemLifetimeListenerMock.contextDestroyed(sceMock);
+        verify(runtimeMock, times(2)).removeShutdownHook(any());
     }
 }
