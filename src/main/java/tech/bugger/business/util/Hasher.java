40595afee140c32ca059b163d8517dd45e1a915d
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2020-12-27 01:30:54 +0100

    Implement Hashing

--- src/main/java/tech/bugger/business/util/Hasher.java
+++ src/main/java/tech/bugger/business/util/Hasher.java
@@ -1,37 +1,126 @@
 package tech.bugger.business.util;
 
-import java.util.logging.Logger;
+import tech.bugger.business.exception.CryptographyImpossibleException;
+import tech.bugger.global.util.Log;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Utility class for cryptographic hashing.
  */
 public final class Hasher {
-    private static final Logger logger = Logger.getLogger(Hasher.class.getName());
 
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
+    private static final Log log = Log.forClass(Hasher.class);
+
+    /**
+     * Cryptographically secure random generator.
+     */
+    private static final SecureRandom random = new SecureRandom();
+
+    /**
+     * Cache of hashing algorithm instances by name.
+     */
+    private static final Map<String, MessageDigest> algorithms = new HashMap<>();
+
+    /**
+     * The hexadecimal basis.
+     */
+    public static final int HEX_RADIX = 16;
+
+    /**
+     * Number of bits in half a byte.
+     */
+    public static final int HALF_BYTE = 4;
+
+    /**
+     * Prevents instantiation of this utility class.
+     */
     private Hasher() {
+        throw new UnsupportedOperationException(); // for reflection abusers
     }
 
     /**
-     * Hash the given string with the given algorithm and salt.
+     * Hashes the given string with the given salt using the provided algorithm.
+     *
+     * The procedure can be described as {@code algo(input.salt)} where the dot (.) means concatenation.
      *
      * @param input The string to be hashed and salted.
-     * @param salt  The salt to be applied before hashing.
-     * @param algo  The hashing algorithm to use. Available algorithms are described in the  <a href=
+     * @param salt  The salt to be appended before hashing as hexadecimal string of format {@code ([0-9a-f]{2})*}.
+     * @param algo  The hashing algorithm to use. Available algorithms are described in the <a href=
      *              "https://docs.oracle.com/javase/8/docs/technotes/guides/security/StandardNames.html#MessageDigest">
-     *              MessageDigest section</a>
-     * @return The hash value of {@code string} under the given parameters as hexadecimal string.
+     *              MessageDigest section</a> of the Java Security API documentation.
+     * @return The hash value of {@code input} as hexadecimal string of format {@code ([0-9a-f]{2})*}.
      */
     public static String hash(final String input, final String salt, final String algo) {
-        return null;
+        if (input == null) {
+            throw new IllegalArgumentException("Input to hash must not be null.");
+        } else if (salt == null) {
+            throw new IllegalArgumentException("Salt must not be null.");
+        } else if (algo == null) {
+            throw new IllegalArgumentException("Hashing algorithm must not be null.");
+        }
+
+        byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
+        byte[] saltBytes = hexToBytes(salt);
+        MessageDigest algorithm = getAlgorithm(algo);
+        algorithm.update(inputBytes);
+        algorithm.update(saltBytes);
+        return bytesToHex(algorithm.digest());
     }
 
     /**
-     * Generate a random salt with the given length.
+     * Generates the given number of random bytes in hexadecimal representation.
      *
-     * @param numBytes The desired length of the salt in bytes.
-     * @return Hexadecimal representation of the salt.
+     * @param numBytes The desired number of bytes to randomly generate.
+     * @return {@code numBytes} random bytes as hexadecimal string of format {@code ([0-9a-f]{2})*}.
      */
-    public static String generateSalt(int numBytes) {
-        return null;
+    public static String generateRandomBytes(final int numBytes) {
+        byte[] bytes = new byte[numBytes];
+        random.nextBytes(bytes);
+        return bytesToHex(bytes);
+    }
+
+    private static MessageDigest getAlgorithm(final String algo) {
+        if (!algorithms.containsKey(algo)) {
+            try {
+                algorithms.put(algo, MessageDigest.getInstance(algo));
+            } catch (NoSuchAlgorithmException e) {
+                log.error(algo + " not supported!", e);
+                throw new CryptographyImpossibleException(e);
+            }
+        }
+        return algorithms.get(algo);
+    }
+
+    private static String bytesToHex(final byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    private static byte[] hexToBytes(final String hex) {
+        if (hex.length() % 2 != 0) {
+            throw new IllegalArgumentException("Hex string must have even length!");
+        }
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int upperByte = Character.digit(hex.charAt(2 * i), HEX_RADIX);
+            int lowerByte = Character.digit(hex.charAt(2 * i + 1), HEX_RADIX);
+            if (upperByte == -1 || lowerByte == -1) {
+                throw new IllegalArgumentException("Hex string must only contain [0-9a-f]!");
+            }
+            bytes[i] = (byte) ((upperByte << HALF_BYTE) + lowerByte);
+        }
+        return bytes;
     }
 }
---
+++ src/test/java/tech/bugger/business/util/HasherTest.java
@@ -0,0 +1,84 @@
+package tech.bugger.business.util;
+
+import org.junit.jupiter.api.Test;
+import tech.bugger.business.exception.CryptographyImpossibleException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HasherTest {
+    private static final String INPUT = "Hash my darling, don't fear my darling.";
+    private static final String SALT = "2f73616c7421"; // equals "/salt!" in UTF-8
+    private static final String ALGO = "SHA3-512";
+    public static final int NUM_BYTES = 42;
+
+
+    @Test
+    public void testConstructorAccess() throws NoSuchMethodException {
+        Constructor<Hasher> constructor = Hasher.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Throwable e = assertThrows(InvocationTargetException.class, constructor::newInstance);
+        assertEquals(UnsupportedOperationException.class, e.getCause().getClass());
+    }
+
+    @Test
+    public void testHashWhenInputNull() {
+        assertThrows(IllegalArgumentException.class, () -> Hasher.hash(null, SALT, ALGO));
+    }
+
+    @Test
+    public void testHashWhenSaltNull() {
+        assertThrows(IllegalArgumentException.class, () -> Hasher.hash(INPUT, null, ALGO));
+    }
+
+    @Test
+    void testHashWhenSaltUnevenLength() {
+        assertThrows(IllegalArgumentException.class, () -> Hasher.hash(INPUT, "a0f", ALGO));
+    }
+
+    @Test
+    void testHashWhenSaltInvalidHexString() {
+        assertThrows(IllegalArgumentException.class, () -> Hasher.hash(INPUT, "0x123ö", ALGO));
+    }
+
+    @Test
+    void testHashWhenSaltAnotherInvalidHexString() {
+        assertThrows(IllegalArgumentException.class, () -> Hasher.hash(INPUT, "äü", ALGO));
+    }
+
+    @Test
+    public void testHashWhenAlgorithmNull() {
+        assertThrows(IllegalArgumentException.class, () -> Hasher.hash(INPUT, SALT, null));
+    }
+
+    @Test
+    public void testHashWhenAlgorithmNotAvailable() {
+        assertThrows(CryptographyImpossibleException.class, () -> Hasher.hash(INPUT, SALT, "SCHA-512"));
+    }
+
+    @Test
+    public void testHashWithoutSalt() {
+        assertEquals("a2ab83ebe527d4d6336116d4af9335e91c0c59225f5a98ce24ff43065bbd222da25eb43b7a8cee3890bad7f46e624c6b0"
+                + "fd892f94adfe71c7d2beb0b7fa0fa13", Hasher.hash(INPUT, "", ALGO));
+    }
+
+    @Test
+    public void testHashWithSalt() {
+        assertEquals("78ae52530f31daf36e222f970c7888bd75bc5ceca4025dcbf0352fbc196cc240b874918dc6e87a122bfcf190dc58c000e"
+                + "b4a48449b30192ddb88a53fdf833b35", Hasher.hash(INPUT, SALT, ALGO));
+    }
+
+    @Test
+    public void testGenerateRandomBytes() {
+        String bytes = Hasher.generateRandomBytes(NUM_BYTES);
+        assertAll(
+                () -> assertTrue(bytes.matches("([0-9a-f]{2})*")),
+                () -> assertEquals(NUM_BYTES * 2, bytes.length())
+        );
+    }
+}
