4f9757b663057c5860a8f78d7958ebc0c9f59737
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2020-12-28 23:27:29 +0100

    Optimize Feedback Framework

---
+++ src/main/java/tech/bugger/control/util/FeedbackObserver.java
@@ -0,0 +1,45 @@
+package tech.bugger.control.util;
+
+import tech.bugger.business.util.Feedback;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Any;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+/**
+ * Observer for service feedback messages.
+ */
+public class FeedbackObserver {
+
+    /**
+     * The current {@link FacesContext}.
+     */
+    private final FacesContext fctx;
+
+    /**
+     * Constructs a new feedback observer.
+     *
+     * @param fctx A reference to the {@link FacesContext}.
+     */
+    @Inject
+    public FeedbackObserver(final FacesContext fctx) {
+        this.fctx = fctx;
+    }
+
+    /**
+     * Creates a FacesMessage to display if an event is fired in one of the injected services.
+     *
+     * @param feedback The feedback with details on what to display.
+     */
+    public void displayFeedback(final @Observes @Any Feedback feedback) {
+        FacesMessage.Severity severity = switch (feedback.getType()) {
+            case ERROR -> FacesMessage.SEVERITY_ERROR;
+            case WARNING -> FacesMessage.SEVERITY_WARN;
+            case INFO -> FacesMessage.SEVERITY_INFO;
+        };
+        fctx.addMessage(null, new FacesMessage(severity, feedback.getMessage(), null));
+    }
+
+}
