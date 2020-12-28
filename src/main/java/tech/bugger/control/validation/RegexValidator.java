da3634b5e13a5467152176d509a7bc47736ca6c2
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2020-12-28 13:18:53 +0100

    Implement Regex Validation

--- src/main/java/tech/bugger/control/validation/RegexValidator.java
+++ src/main/java/tech/bugger/control/validation/RegexValidator.java
@@ -1,20 +1,30 @@
 package tech.bugger.control.validation;
 
-import tech.bugger.global.util.Log;
+import tech.bugger.business.util.RegistryKey;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * Validator for regular expression inputs.
  */
-@FacesValidator(value = "regexValidator")
+@FacesValidator(value = "regexValidator", managed = true)
 public class RegexValidator implements Validator<String> {
 
-    private static Log log = Log.forClass(RegexValidator.class);
+    /**
+     * Resource bundle for feedback messages.
+     */
+    @Inject
+    @RegistryKey("messages")
+    private ResourceBundle resourceBundle;
 
     /**
      * Validates the given {@code regex}.
@@ -25,6 +35,13 @@ public class RegexValidator implements Validator<String> {
      * @throws ValidatorException If validation fails.
      */
     @Override
-    public void validate(FacesContext fctx, UIComponent component, String regex) {
+    public void validate(final FacesContext fctx, final UIComponent component, final String regex) {
+        try {
+            Pattern.compile(regex);
+        } catch (PatternSyntaxException e) {
+            FacesMessage message = new FacesMessage(resourceBundle.getString("validation.invalid_regex"));
+            throw new ValidatorException(message, e);
+        }
     }
+
 }
