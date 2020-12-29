4378421f1ad63e203b7bc58fc57b1e4d2eb09eec
Nico Mexis <nico.mexis@kabelmail.de>
2020-12-29 23:40:38 +0100

    Added more test cases and renamed some

--- src/test/java/tech/bugger/business/util/MarkdownTest.java
+++ src/test/java/tech/bugger/business/util/MarkdownTest.java
@@ -65,19 +65,36 @@ public class MarkdownTest {
     @Test
     public void testLinkString() {
         String keyword = "I am a Header. Do you want to go out with me?";
-        String linkTo = "https://example.com/";
-        String md = "[" + keyword + "](" + linkTo + ")";
+        String url = "https://example.com/";
+        String md = "[" + keyword + "](" + url + ")";
         String html = MarkdownHandler.toHtml(md);
-        assertEquals("<p><a href=\"" + linkTo + "\">" + keyword + "</a></p>\n", html);
+        assertEquals("<p><a href=\"" + url + "\">" + keyword + "</a></p>\n", html);
     }
 
     @Test
-    public void testCombinedString() {
+    public void testCombinedHeaderAndLinkString() {
         String keyword = "I am a Header. Do you want to go out with me?";
-        String linkTo = "https://example.com/";
-        String md = "## [" + keyword + "](" + linkTo + ")";
+        String url = "https://example.com/";
+        String md = "## [" + keyword + "](" + url + ")";
         String html = MarkdownHandler.toHtml(md);
-        assertEquals("<h2><a href=\"" + linkTo + "\">" + keyword + "</a></h2>\n", html);
+        assertEquals("<h2><a href=\"" + url + "\">" + keyword + "</a></h2>\n", html);
+    }
+
+    @Test
+    public void testCombinedUserAndReportReferenceString() {
+        String keyword1 = "hyperspeeed";
+        String keyword2 = "42069";
+        String md = "Hello @" + keyword1 + " and goodbye #" + keyword2 + "!";
+        String html = MarkdownHandler.toHtml(md);
+        assertEquals("<p>Hello <a href=\"/user/" + keyword1 + "\" title=\"" + keyword1 + "\">@" + keyword1 + "</a> and goodbye <a href=\"/report/" + keyword2 + "\" title=\"" + keyword2 + "\">#" + keyword2 + "</a>!</p>\n", html);
+    }
+
+    @Test
+    public void testCombinedHeaderAndPostReferenceString() {
+        String keyword = "42069";
+        String md = "## Hello ##" + keyword + " and hello world!";
+        String html = MarkdownHandler.toHtml(md);
+        assertEquals("<h2>Hello <a href=\"/post/" + keyword + "\" title=\"" + keyword + "\">##" + keyword + "</a> and hello world!</h2>\n", html);
     }
 
     @Test
@@ -90,11 +107,27 @@ public class MarkdownTest {
     }
 
     @Test
-    public void testInvalidUserString() {
+    public void testTooShortUserString() {
         String keyword = "+dhas";
         String md = "@" + keyword;
         String html = MarkdownHandler.toHtml(md);
         assertEquals("<p>@" + keyword + "</p>\n", html);
+    }
+
+    @Test
+    public void testTooShortPostString() {
+        String keyword = "+423";
+        String md = "##" + keyword;
+        String html = MarkdownHandler.toHtml(md);
+        assertEquals("<p>##" + keyword + "</p>\n", html);
+    }
+
+    @Test
+    public void testTooShortReportString() {
+        String keyword = "+634";
+        String md = "#" + keyword;
+        String html = MarkdownHandler.toHtml(md);
+        assertEquals("<p>#" + keyword + "</p>\n", html);
     }
 
     @Test
