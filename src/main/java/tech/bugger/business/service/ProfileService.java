2ca7c0f03430540ba1cf5d66d954f3f1573899be
caspar07 <casparil@fim.uni-passau.de>
2020-12-29 23:15:45 +0100

    Add profile service and test

--- src/main/java/tech/bugger/business/service/ProfileService.java
+++ src/main/java/tech/bugger/business/service/ProfileService.java
@@ -1,15 +1,20 @@
 package tech.bugger.business.service;
 
 import tech.bugger.business.util.Feedback;
+import tech.bugger.business.util.RegistryKey;
 import tech.bugger.global.transfer.Report;
 import tech.bugger.global.transfer.Topic;
 import tech.bugger.global.transfer.User;
 import tech.bugger.global.util.Log;
+import tech.bugger.persistence.exception.NotFoundException;
+import tech.bugger.persistence.exception.TransactionException;
+import tech.bugger.persistence.util.Transaction;
+import tech.bugger.persistence.util.TransactionManager;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
-import javax.enterprise.inject.Any;
 import javax.inject.Inject;
+import java.util.ResourceBundle;
 
 /**
  * Service providing methods related to users and user profiles. A {@code Feedback} event is fired, if unexpected
@@ -18,11 +23,25 @@ import javax.inject.Inject;
 @Dependent
 public class ProfileService {
 
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
     private static final Log log = Log.forClass(ProfileService.class);
 
+    private Event<Feedback> feedback;
+
+    private TransactionManager transactionManager;
+
+    @RegistryKey("messages")
+    private ResourceBundle messages;
+
     @Inject
-    @Any
-    Event<Feedback> feedback;
+    public ProfileService(final Event<Feedback> feedback, final TransactionManager transactionManager,
+                          final ResourceBundle messages) {
+        this.feedback = feedback;
+        this.transactionManager = transactionManager;
+        this.messages = messages;
+    }
 
     /**
      * Returns the user with the specified ID. If no such user exists, returns {@code null} and fires an event.
@@ -31,7 +50,18 @@ public class ProfileService {
      * @return The user, if they exist, {@code null} if no user with that ID exists.
      */
     public User getUser(int id) {
-        return null;
+        User user = null;
+        try(Transaction transaction = transactionManager.begin()) {
+            user = transaction.newUserGateway().getUserByID(id);
+            transaction.commit();
+        } catch (NotFoundException e) {
+            log.error("The user could not be found.", e);
+            feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+        } catch (TransactionException e) {
+            log.error("Error while loading the user.", e);
+            feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+        }
+        return user;
     }
 
     /**
@@ -57,7 +87,16 @@ public class ProfileService {
      * @param user The user to update.
      */
     public void updateUser(User user) {
-
+        try(Transaction transaction = transactionManager.begin()) {
+            transaction.newUserGateway().updateUser(user);
+            transaction.commit();
+        } catch (NotFoundException e) {
+            log.error("The user could not be found.", e);
+            feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+        } catch (TransactionException e) {
+            log.error("Error while updating the user.", e);
+            feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+        }
     }
 
     /**
@@ -140,7 +179,18 @@ public class ProfileService {
      * @return The voting weight as an {@code int}.
      */
     public int getVotingWeightForUser(User user) {
-        return 0;
+        int votingWeight = 0;
+        try(Transaction transaction = transactionManager.begin()) {
+            votingWeight = transaction.newUserGateway().getVotingWeight(user);
+            transaction.commit();
+        } catch (NotFoundException e) {
+            log.error("The user could not be found.", e);
+            feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+        } catch (TransactionException e) {
+            log.error("Error while calculating the user's voting weight.", e);
+            feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+        }
+        return votingWeight;
     }
 
     /**
@@ -150,7 +200,88 @@ public class ProfileService {
      * @return The number of posts as an {@code int}.
      */
     public int getNumberOfPostsForUser(User user) {
-        return 0;
+        int numPosts = 0;
+        try(Transaction transaction = transactionManager.begin()) {
+            numPosts = transaction.newUserGateway().getNumberOfPosts(user);
+            transaction.commit();
+        } catch (NotFoundException e) {
+            log.error("The user could not be found.", e);
+            feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+        } catch (TransactionException e) {
+            log.error("Error while calculating the user's voting weight.", e);
+            feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+        }
+        return numPosts;
+    }
+
+    /**
+     * Promotes the user whose profile is being viewed to an administrator or demotes the user whose profile is being
+     * viewed if they are an administrator. However, if they are the last remaining administrator, a feedback event is
+     * fired instead.
+     * @param user The user to be promoted/demoted.
+     */
+    public void toggleAdmin(User user) {
+        if (user.isAdministrator()) {
+            try (Transaction transaction = transactionManager.begin()) {
+                int admins = transaction.newUserGateway().getNumberOfAdmins();
+                transaction.commit();
+                transaction.close();
+                if (admins > 1) {
+                    demoteAdmin(user);
+                } else {
+                    log.error("The last administrator cannot be deleted");
+                    feedback.fire(new Feedback(messages.getString("delete_last_admin"), Feedback.Type.ERROR));
+                }
+            } catch (NotFoundException e) {
+                log.error("No administrators could be found.", e);
+                feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+            } catch (TransactionException e) {
+                log.error("Error while counting the number of administrators.", e);
+                feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+            }
+        } else {
+            promoteAdmin(user);
+        }
+    }
+
+    /**
+     * Promotes the given user to an administrator.
+     * @param user The user to be promoted.
+     */
+    private void promoteAdmin(User user) {
+        try(Transaction transaction = transactionManager.begin()) {
+            user.setAdministrator(true);
+            transaction.newUserGateway().updateUser(user);
+            transaction.commit();
+        } catch (NotFoundException e) {
+            user.setAdministrator(false);
+            log.error("The user could not be found.", e);
+            feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+        } catch (TransactionException e) {
+            user.setAdministrator(false);
+            log.error("Error while updating the user.", e);
+            feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+        }
+    }
+
+    /**
+     * Deprives the given user of his administrator status.
+     * @param user The user to be demoted.
+     */
+    private void demoteAdmin(User user) {
+        try(Transaction transaction = transactionManager.begin()) {
+            user.setAdministrator(false);
+            transaction.newUserGateway().updateUser(user);
+            transaction.commit();
+        } catch (NotFoundException e) {
+            user.setAdministrator(true);
+            log.error("The user could not be found.", e);
+            feedback.fire(new Feedback(messages.getString("not_found_error"), Feedback.Type.ERROR));
+        } catch (TransactionException e) {
+            user.setAdministrator(true);
+            log.error("Error while updating the user.", e);
+            feedback.fire(new Feedback(messages.getString("data_access_error"), Feedback.Type.ERROR));
+        }
     }
 
     private byte[] generateThumbnail(byte[] image) {
---
+++ src/test/java/tech/bugger/business/service/ProfileServiceTest.java
@@ -0,0 +1,289 @@
+package tech.bugger.business.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tech.bugger.LogExtension;
+import tech.bugger.business.util.Feedback;
+import tech.bugger.global.transfer.User;
+import tech.bugger.persistence.exception.NotFoundException;
+import tech.bugger.persistence.exception.TransactionException;
+import tech.bugger.persistence.gateway.UserDBGateway;
+import tech.bugger.persistence.util.Transaction;
+import tech.bugger.persistence.util.TransactionManager;
+
+import javax.enterprise.event.Event;
+import java.time.ZonedDateTime;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.any;
+
+@ExtendWith(LogExtension.class)
+public class ProfileServiceTest {
+
+    @Mock
+    Event<Feedback> feedback;
+
+    @Mock
+    private TransactionManager transactionManager;
+
+    @Mock
+    ResourceBundle messages;
+
+    @Mock
+    private Transaction transaction;
+
+    @Mock
+    private UserDBGateway gateway;
+
+    private ProfileService profileService;
+
+    private User user;
+    private final int theAnswer = 42;
+
+    @BeforeEach
+    public void setup()
+    {
+        user = new User();
+        user.setId(12345);
+        user.setUsername("Helgi");
+        user.setPasswordHash("v3ry_s3cur3");
+        user.setEmailAddress("helga@web.de");
+        user.setFirstName("Helga");
+        user.setLastName("Brötchen");
+        user.setBiography("Hallo, ich bin die Helgi | Perfect | He/They/Her | vergeben | Abo =|= endorsement)");
+        user.setProfileVisibility(User.ProfileVisibility.MINIMAL);
+        user.setRegistrationDate(ZonedDateTime.now());
+        user.setAdministrator(false);
+        MockitoAnnotations.openMocks(this);
+        profileService = new ProfileService(feedback, transactionManager, messages);
+        when(transactionManager.begin()).thenReturn(transaction);
+        when(transaction.newUserGateway()).thenReturn(gateway);
+    }
+
+    @Test
+    public void testGetUser() {
+        try {
+            when(gateway.getUserByID(user.getId())).thenReturn(user);
+            User getUser = profileService.getUser(user.getId());
+            assertEquals(user.getId(), getUser.getId());
+            verify(gateway, times(1)).getUserByID(user.getId());
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testGetUserNotFound() throws NotFoundException {
+        when(gateway.getUserByID(user.getId())).thenThrow(NotFoundException.class);
+        assertNull(profileService.getUser(user.getId()));
+        verify(gateway, times(1)).getUserByID(user.getId());
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testGetUserTransactionException() throws TransactionException {
+        doThrow(TransactionException.class).when(transaction).commit();
+        assertNull(profileService.getUser(user.getId()));
+        verify(transaction, times(1)).commit();
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        try {
+            profileService.updateUser(user);
+            verify(gateway, times(1)).updateUser(user);
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testUpdateUserNotFound() throws NotFoundException {
+        doThrow(NotFoundException.class).when(gateway).updateUser(user);
+        profileService.updateUser(user);
+        verify(gateway, times(1)).updateUser(user);
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testUpdateUserTransactionException() throws TransactionException {
+        doThrow(TransactionException.class).when(transaction).commit();
+        profileService.updateUser(user);
+        verify(transaction, times(1)).commit();
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testGetVotingWeight() {
+        try {
+            when(gateway.getVotingWeight(user)).thenReturn(theAnswer);
+            assertEquals(42, profileService.getVotingWeightForUser(user));
+            verify(gateway, times(1)).getVotingWeight(user);
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testGetVotingWeightNotFound() throws NotFoundException {
+        when(gateway.getVotingWeight(user)).thenThrow(NotFoundException.class);
+        assertEquals(0, profileService.getVotingWeightForUser(user));
+        verify(gateway, times(1)).getVotingWeight(user);
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testGetVotingWeightTransactionException() throws TransactionException {
+        doThrow(TransactionException.class).when(transaction).commit();
+        assertEquals(0, profileService.getVotingWeightForUser(user));
+        verify(transaction, times(1)).commit();
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testGetNumberOfPosts() {
+        try {
+            when(gateway.getNumberOfPosts(user)).thenReturn(theAnswer);
+            assertEquals(42, profileService.getNumberOfPostsForUser(user));
+            verify(gateway, times(1)).getNumberOfPosts(user);
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testGetNumberOfPostsNotFound() throws NotFoundException {
+        when(gateway.getNumberOfPosts(user)).thenThrow(NotFoundException.class);
+        assertEquals(0, profileService.getNumberOfPostsForUser(user));
+        verify(gateway, times(1)).getNumberOfPosts(user);
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testGetNumberOfPostsTransactionException() throws TransactionException {
+        doThrow(TransactionException.class).when(transaction).commit();
+        assertEquals(0, profileService.getNumberOfPostsForUser(user));
+        verify(transaction, times(1)).commit();
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testToggleAdminPromote() {
+        try {
+            profileService.toggleAdmin(user);
+            assertTrue(user.isAdministrator());
+            verify(gateway, times(1)).updateUser(user);
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testToggleAdminPromoteNotFound() throws NotFoundException {
+        doThrow(NotFoundException.class).when(gateway).updateUser(user);
+        profileService.toggleAdmin(user);
+        assertFalse(user.isAdministrator());
+        verify(gateway, times(1)).updateUser(user);
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testToggleAdminPromoteTransactionException() throws TransactionException {
+        doThrow(TransactionException.class).when(transaction).commit();
+        profileService.toggleAdmin(user);
+        assertFalse(user.isAdministrator());
+        verify(transaction, times(1)).commit();
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testToggleAdminDemoteAdmin() {
+        try {
+            user.setAdministrator(true);
+            when(gateway.getNumberOfAdmins()).thenReturn(theAnswer);
+            profileService.toggleAdmin(user);
+            assertFalse(user.isAdministrator());
+            verify(gateway, times(1)).getNumberOfAdmins();
+            verify(gateway, times(1)).updateUser(user);
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testToggleAdminDemoteAdminNotFound() throws NotFoundException {
+        user.setAdministrator(true);
+        when(gateway.getNumberOfAdmins()).thenReturn(theAnswer);
+        doThrow(NotFoundException.class).when(gateway).updateUser(user);
+        profileService.toggleAdmin(user);
+        assertTrue(user.isAdministrator());
+        verify(gateway, times(1)).getNumberOfAdmins();
+        verify(gateway, times(1)).updateUser(user);
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testToggleAdminDemoteAdminTransactionException() throws TransactionException {
+        try {
+            user.setAdministrator(true);
+            when(gateway.getNumberOfAdmins()).thenReturn(theAnswer);
+            doNothing().doThrow(TransactionException.class).when(transaction).commit();
+            profileService.toggleAdmin(user);
+            assertTrue(user.isAdministrator());
+            verify(gateway, times(1)).getNumberOfAdmins();
+            verify(gateway, times(1)).updateUser(user);
+            verify(transaction, times(2)).commit();
+            verify(feedback, times(1)).fire(any());
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testToggleAdminLastAdmin() {
+        try {
+            user.setAdministrator(true);
+            when(gateway.getNumberOfAdmins()).thenReturn(1);
+            profileService.toggleAdmin(user);
+            assertTrue(user.isAdministrator());
+            verify(gateway, times(1)).getNumberOfAdmins();
+            verify(feedback, times(1)).fire(any());
+        } catch (NotFoundException e) {
+            fail("The gateway has falsely thrown a NotFoundException!");
+        }
+    }
+
+    @Test
+    public void testToggleAdminNotFound() throws NotFoundException {
+        user.setAdministrator(true);
+        doThrow(NotFoundException.class).when(gateway).getNumberOfAdmins();
+        profileService.toggleAdmin(user);
+        assertTrue(user.isAdministrator());
+        verify(gateway, times(1)).getNumberOfAdmins();
+        verify(feedback, times(1)).fire(any());
+    }
+
+    @Test
+    public void testToggleAdminTransactionException() throws TransactionException {
+        user.setAdministrator(true);
+        doThrow(TransactionException.class).when(transaction).commit();
+        profileService.toggleAdmin(user);
+        assertTrue(user.isAdministrator());
+        verify(transaction, times(1)).commit();
+        verify(feedback, times(1)).fire(any());
+    }
+}
