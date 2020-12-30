573f0d2c2619030e90a057b87a46b823b7a4b464
Nico Mexis <nico.mexis@kabelmail.de>
2020-12-30 13:48:34 +0100

    Format UserSession and add utility function to Language

--- src/main/java/tech/bugger/business/internal/UserSession.java
+++ src/main/java/tech/bugger/business/internal/UserSession.java
@@ -1,15 +1,14 @@
 package tech.bugger.business.internal;
 
-import tech.bugger.global.transfer.User;
-import tech.bugger.global.util.Log;
-
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Locale;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import java.io.Serial;
-import java.io.Serializable;
-import java.util.Locale;
+import tech.bugger.global.transfer.User;
+import tech.bugger.global.util.Log;
 
 /**
  * Internal bean holding details of the session of a user.
@@ -17,12 +16,28 @@ import java.util.Locale;
 @SessionScoped
 @Named
 public class UserSession implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 8943571923172893158L;
+
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
     private static final Log log = Log.forClass(UserSession.class);
+
+    /**
+     * The currently logged in user.
+     */
     private User user;
+
+    /**
+     * The currently selected locale.
+     */
     private Locale locale;
 
+    /**
+     * Initializes this user session by setting a default locale.
+     */
     @PostConstruct
     public void init() {
         locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
@@ -32,29 +47,28 @@ public class UserSession implements Serializable {
      * Invalidates the session.
      */
     public void invalidateSession() {
-
     }
 
     /**
-     * Gets the user.
+     * Gets the user or {@code null} if he is not logged in.
      *
-     * @return The user.
+     * @return The user or {@code null} if he is not logged in.
      */
     public User getUser() {
         return user;
     }
 
     /**
-     * Sets the user.
+     * Sets the current user.
      *
      * @param user The user to set.
      */
-    public void setUser(User user) {
+    public void setUser(final User user) {
         this.user = user;
     }
 
     /**
-     * Gets the locale.
+     * Gets the currently preferred locale.
      *
      * @return The locale.
      */
@@ -63,11 +77,11 @@ public class UserSession implements Serializable {
     }
 
     /**
-     * Sets the locale.
+     * Sets the preferred locale.
      *
-     * @param locale The locale to set.
+     * @param locale The preferred locale to set.
      */
-    public void setLocale(Locale locale) {
+    public void setLocale(final Locale locale) {
         this.locale = locale;
     }
 
