6fdc491b6a49ce1e16956f830b6cc21244ef5cc1
Ben <huberb@fim.uni-passau.de>
2020-12-30 17:38:16 +0100

    Add constructor and some fields

--- src/main/java/tech/bugger/business/service/TopicService.java
+++ src/main/java/tech/bugger/business/service/TopicService.java
@@ -1,18 +1,20 @@
 package tech.bugger.business.service;
 
 import tech.bugger.business.util.Feedback;
+import tech.bugger.business.util.RegistryKey;
 import tech.bugger.global.transfer.Report;
 import tech.bugger.global.transfer.Selection;
 import tech.bugger.global.transfer.Topic;
 import tech.bugger.global.transfer.User;
 import tech.bugger.global.util.Log;
+import tech.bugger.persistence.util.TransactionManager;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
-import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * Service providing methods related to topics. A {@code Feedback} event is fired, if unexpected circumstances occur.
@@ -20,11 +22,40 @@ import java.util.List;
 @Dependent
 public class TopicService {
 
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
     private static final Log log = Log.forClass(TopicService.class);
 
+    /**
+     * Transaction manager used for creating transactions.
+     */
+    private final TransactionManager transactionManager;
+
+    /**
+     * Feedback Event for user feedback.
+     */
+    private final Event<Feedback> feedbackEvent;
+
+    /**
+     * Resource bundle for feedback messages.
+     */
+    private final ResourceBundle messagesBundle;
+
+    /**
+     * Constructs a new topic service with the given dependencies.
+     *
+     * @param transactionManager The transaction manager to use for creating transactions.
+     * @param feedbackEvent The feedback event to use for user feedback.
+     * @param messagesBundle The resource bundle for feedback messages.
+     */
     @Inject
-    @Any
-    Event<Feedback> feedback;
+    public TopicService(final TransactionManager transactionManager, final Event<Feedback> feedbackEvent,
+                        final @RegistryKey("messages") ResourceBundle messagesBundle) {
+        this.transactionManager = transactionManager;
+        this.feedbackEvent = feedbackEvent;
+        this.messagesBundle = messagesBundle;
+    }
 
     /**
      * Bans a user from a topic. Administrators and moderators of the topic cannot be banned.
