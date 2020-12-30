1a5e2a0cce26925a3861f6d15480b8a1866d7e69
Nico Mexis <nico.mexis@kabelmail.de>
2020-12-30 14:38:16 +0100

    Tests for PasswordValidatorTest

---
+++ src/test/java/tech/bugger/control/validation/PasswordValidatorTest.java
@@ -0,0 +1,51 @@
+package tech.bugger.control.validation;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.bugger.ResourceBundleMocker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PasswordValidatorTest {
+
+    private PasswordValidator passwordValidator;
+
+    @Mock
+    private FacesContext fctx;
+
+    @Mock
+    private UIComponent comp;
+
+    @BeforeEach
+    public void setUp() {
+        passwordValidator = new PasswordValidator(ResourceBundleMocker.mock(""));
+    }
+
+    @Test
+    public void testValidateOnTooShortPassword() {
+        assertThrows(ValidatorException.class, () -> passwordValidator.validate(fctx, comp, "t3st!"));
+    }
+
+    @Test
+    public void testValidateOnTooLongPassword() {
+        assertThrows(ValidatorException.class, () -> passwordValidator.validate(fctx, comp, "Hello1am4verystr0ngpassword!hello1am4verystr0ngpassword!hello1am4verystr0ngpassword!hello1am4verystr0ngpassword!hello1am4verystr0ngpassworD!"));
+    }
+
+    @Test
+    public void testValidateOnTooWeakPassword() {
+        assertThrows(ValidatorException.class, () -> passwordValidator.validate(fctx, comp, "hello1am4wEakpassword"));
+    }
+
+    @Test
+    public void testValidateOnGoodPassword() {
+        assertDoesNotThrow(() -> passwordValidator.validate(fctx, comp, "Hello1am4verystr0ngpassword!"));
+    }
+
+}
