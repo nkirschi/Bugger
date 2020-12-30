c68387841456eeab9a22b1181cc951a7047b9682
Nico Mexis <nico.mexis@kabelmail.de>
2020-12-30 13:47:26 +0100

    Finish User Gateway

--- src/main/java/tech/bugger/persistence/gateway/UserDBGateway.java
+++ src/main/java/tech/bugger/persistence/gateway/UserDBGateway.java
@@ -1,29 +1,45 @@
 package tech.bugger.persistence.gateway;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.time.ZoneId;
+import java.util.List;
+import tech.bugger.global.transfer.Language;
 import tech.bugger.global.transfer.Report;
 import tech.bugger.global.transfer.Selection;
 import tech.bugger.global.transfer.Topic;
 import tech.bugger.global.transfer.User;
+import tech.bugger.global.util.Lazy;
 import tech.bugger.global.util.Log;
-
-import java.sql.Connection;
-import java.util.List;
+import tech.bugger.persistence.exception.NotFoundException;
+import tech.bugger.persistence.exception.StoreException;
+import tech.bugger.persistence.util.StatementParametrizer;
 
 /**
  * User gateway that gives access to user stored in a database.
  */
 public class UserDBGateway implements UserGateway {
 
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
     private static final Log log = Log.forClass(UserDBGateway.class);
 
-    private Connection conn;
+    /**
+     * Database connection used by this gateway.
+     */
+    private final Connection conn;
 
     /**
      * Constructs a new user gateway with the given database connection.
      *
      * @param conn The database connection to use for the gateway.
      */
-    public UserDBGateway(Connection conn) {
+    public UserDBGateway(final Connection conn) {
         this.conn = conn;
     }
 
@@ -31,7 +47,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public boolean isModerator(User user, Topic topic) {
+    public boolean isModerator(final User user, final Topic topic) {
         // TODO Auto-generated method stub
         return false;
     }
@@ -49,7 +65,36 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public User getUserByID(int id) {
+    public User getUserByID(final int id) throws NotFoundException {
+        try (PreparedStatement stmt = conn.prepareStatement("SELECT * FROM \"user\" WHERE id = ?")) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new User(rs.getInt("id"), rs.getString("username"),
+                        rs.getString("password_hash"), rs.getString("password_salt"),
+                        rs.getString("hashing_algorithm"), rs.getString("email_address"),
+                        rs.getString("first_name"), rs.getString("last_name"),
+                        new Lazy<>(rs.getBytes("avatar")), rs.getBytes("avatar_thumbnail"),
+                        rs.getString("biography"), Language.valueOf(rs.getString("preferred_language").toUpperCase()),
+                        User.ProfileVisibility.valueOf(rs.getString("profile_visibility").toUpperCase()),
+                        rs.getTimestamp("registered_at").toLocalDateTime().atZone(ZoneId.systemDefault()),
+                        rs.getInt("forced_voting_weight"), rs.getBoolean("is_admin"));
+            } else {
+                log.error("No user could be found in the database");
+                throw new NotFoundException("No user could be found in the database.");
+            }
+        } catch (SQLException e) {
+            log.error("Error while searching for user", e);
+            throw new StoreException("Error while searching for user", e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User getUserByUsername(final String username) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -58,7 +103,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public User getUserByUsername(String username) {
+    public List<User> getSelectedModerators(final Topic topic, final Selection selection) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -67,7 +112,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public List<User> getSelectedModerators(Topic topic, Selection selection) {
+    public List<User> getSelectedBannedUsers(final Topic topic, final Selection selection) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -76,7 +121,107 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public List<User> getSelectedBannedUsers(Topic topic, Selection selection) {
+    public void createUser(final User user) {
+        try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO \"user\" "
+                        + "(username, password_hash, password_salt, hashing_algorithm, email_address, first_name, "
+                        + "last_name, avatar, avatar_thumbnail, biography, preferred_language, profile_visibility, "
+                        + "forced_voting_weight, is_admin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            Lazy<byte[]> avatar = user.getAvatar();
+
+            new StatementParametrizer(stmt)
+                    .string(user.getUsername())
+                    .string(user.getPasswordHash())
+                    .string(user.getPasswordSalt())
+                    .string(user.getHashingAlgorithm())
+                    .string(user.getEmailAddress())
+                    .string(user.getFirstName())
+                    .string(user.getLastName())
+                    .bytes(avatar.isPresent() ? avatar.get() : new byte[0])
+                    .bytes(user.getAvatarThumbnail())
+                    .string(user.getBiography())
+                    .string(user.getPreferredLanguage().name())
+                    .object(user.getProfileVisibility(), Types.OTHER)
+                    .object(user.getForcedVotingWeight(), Types.INTEGER)
+                    .bool(user.isAdministrator())
+                    .toStatement().executeUpdate();
+
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    user.setId(rs.getInt("id"));
+                    user.setRegistrationDate(rs.getTimestamp("registered_at").toLocalDateTime()
+                            .atZone(ZoneId.systemDefault()));
+                }
+            }
+        } catch (SQLException e) {
+            log.error("Couldn't verify the token's validity due to a database error.", e);
+            throw new StoreException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateUser(final User user) throws NotFoundException {
+        if (user.getId() == null) {
+            throw new IllegalArgumentException("User ID may not be null!");
+        }
+
+        Lazy<byte[]> avatar = user.getAvatar();
+        try (PreparedStatement stmt = conn.prepareStatement("UPDATE \"user\" SET "
+                        + "username = ?, password_hash = ?, password_salt = ?, hashing_algorithm = ?, "
+                        + "email_address = ?, first_name = ?, last_name = ?, avatar_thumbnail = ?, "
+                        + "biography = ?, preferred_language = ?, profile_visibility = ?, "
+                        + "forced_voting_weight = ?, is_admin = ? " + (avatar.isPresent() ? ", avatar = ? " : "")
+                        + "WHERE id = ?",
+                Statement.RETURN_GENERATED_KEYS)) {
+            StatementParametrizer statementParametrizer = new StatementParametrizer(stmt)
+                    .string(user.getUsername())
+                    .string(user.getPasswordHash())
+                    .string(user.getPasswordSalt())
+                    .string(user.getHashingAlgorithm())
+                    .string(user.getEmailAddress())
+                    .string(user.getFirstName())
+                    .string(user.getLastName())
+                    .bytes(user.getAvatarThumbnail())
+                    .string(user.getBiography())
+                    .string(user.getPreferredLanguage().name())
+                    .object(user.getProfileVisibility(), Types.OTHER)
+                    .object(user.getForcedVotingWeight(), Types.INTEGER)
+                    .bool(user.isAdministrator());
+
+            if (avatar.isPresent()) {
+                statementParametrizer.bytes(avatar.get());
+            }
+
+            int changedRows = statementParametrizer
+                    .integer(user.getId())
+                    .toStatement().executeUpdate();
+            if (changedRows == 0) {
+                log.error("No user could be found in the database.");
+                throw new NotFoundException("No user could be found in the database.");
+            }
+        } catch (SQLException e) {
+            log.error("Couldn't verify the token's validity due to a database error.", e);
+            throw new StoreException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deleteUser(final User user) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<User> getSubscribersOf(final User user) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -85,35 +230,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public void createUser(User user) {
-        // TODO Auto-generated method stub
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void updateUser(User user) {
-        // TODO Auto-generated method stub
-        // For Lazy attributes: check if they're there first, if not, don't do anything
-        // with them
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void deleteUser(User user) {
-        // TODO Auto-generated method stub
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<User> getSubscribersOf(User user) {
+    public List<User> getSubscribersOf(final Report report) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -122,7 +239,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public List<User> getSubscribersOf(Report report) {
+    public List<User> getSubscribersOf(final Topic topic) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -131,16 +248,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public List<User> getSubscribersOf(Topic topic) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getNumberOfPosts(User user) {
+    public int getNumberOfPosts(final User user) {
         // TODO Auto-generated method stub
         return 0;
     }
@@ -149,7 +257,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public int getVotingWeight(User user) {
+    public int getVotingWeight(final User user) {
         // TODO Auto-generated method stub
         return 0;
     }
@@ -158,7 +266,7 @@ public class UserDBGateway implements UserGateway {
      * {@inheritDoc}
      */
     @Override
-    public boolean isBanned(User user, Topic topic) {
+    public boolean isBanned(final User user, final Topic topic) {
         // TODO Auto-generated method stub
         return false;
     }
--- src/main/java/tech/bugger/persistence/gateway/UserGateway.java
+++ src/main/java/tech/bugger/persistence/gateway/UserGateway.java
@@ -1,12 +1,11 @@
 package tech.bugger.persistence.gateway;
 
+import java.util.List;
 import tech.bugger.global.transfer.Report;
 import tech.bugger.global.transfer.Selection;
 import tech.bugger.global.transfer.Topic;
 import tech.bugger.global.transfer.User;
 import tech.bugger.persistence.exception.NotFoundException;
-
-import java.util.List;
 
 /**
  * A user gateway allows to query and modify a persistent storage of users.
@@ -21,7 +20,7 @@ public interface UserGateway {
      * @return Whether {@code user} is a moderator of {@code topic}.
      * @throws NotFoundException The user or the topic could not be found.
      */
-    public boolean isModerator(User user, Topic topic) throws NotFoundException;
+    boolean isModerator(User user, Topic topic) throws NotFoundException;
 
     /**
      * Looks up if a user is banned from a given topic.
@@ -31,7 +30,7 @@ public interface UserGateway {
      * @return Whether {@code user} is banned from {@code topic}.
      * @throws NotFoundException The user or the topic could not be found.
      */
-    public boolean isBanned(User user, Topic topic) throws NotFoundException;
+    boolean isBanned(User user, Topic topic) throws NotFoundException;
 
     /**
      * Retrieves the number of posts a user has created.
@@ -40,7 +39,7 @@ public interface UserGateway {
      * @return The number of posts that {@code user} has created.
      * @throws NotFoundException The user could not be found.
      */
-    public int getNumberOfPosts(User user) throws NotFoundException;
+    int getNumberOfPosts(User user) throws NotFoundException;
 
     /**
      * Retrieves the voting weight of a user.
@@ -49,14 +48,14 @@ public interface UserGateway {
      * @return The user's voting weight.
      * @throws NotFoundException The user could not be found.
      */
-    public int getVotingWeight(User user) throws NotFoundException;
+    int getVotingWeight(User user) throws NotFoundException;
 
     /**
      * Retrieves the list of email addresses of all administrators.
      *
      * @return The list of email addresses of all administrators.
      */
-    public List<String> getAdminEmails();
+    List<String> getAdminEmails();
 
     /**
      * Retrieves a user by their ID.
@@ -65,7 +64,7 @@ public interface UserGateway {
      * @return The user identified by the ID.
      * @throws NotFoundException The user could not be found.
      */
-    public User getUserByID(int id) throws NotFoundException;
+    User getUserByID(int id) throws NotFoundException;
 
     /**
      * Retrieves a user by their username.
@@ -74,7 +73,7 @@ public interface UserGateway {
      * @return The user identified by the username.
      * @throws NotFoundException The user could not be found.
      */
-    public User getUserByUsername(String username) throws NotFoundException;
+    User getUserByUsername(String username) throws NotFoundException;
 
     /**
      * Retrieves a list of moderators for a topic that match the given selection criteria.
@@ -84,7 +83,7 @@ public interface UserGateway {
      * @return The list of moderators of {@code topic}, filtered accordingly.
      * @throws NotFoundException The topic could not be found.
      */
-    public List<User> getSelectedModerators(Topic topic, Selection selection) throws NotFoundException;
+    List<User> getSelectedModerators(Topic topic, Selection selection) throws NotFoundException;
 
     /**
      * Retrieves a list of users that are banned from a given topic and match the given selection criteria.
@@ -94,7 +93,7 @@ public interface UserGateway {
      * @return The list of users banned from {@code topic}, filtered accordingly.
      * @throws NotFoundException The topic could not be found.
      */
-    public List<User> getSelectedBannedUsers(Topic topic, Selection selection) throws NotFoundException;
+    List<User> getSelectedBannedUsers(Topic topic, Selection selection) throws NotFoundException;
 
     /**
      * Retrieves the list of subscribers to a user.
@@ -103,7 +102,7 @@ public interface UserGateway {
      * @return The list of subscribers to {@code user}.
      * @throws NotFoundException The user could not be found.
      */
-    public List<User> getSubscribersOf(User user) throws NotFoundException;
+    List<User> getSubscribersOf(User user) throws NotFoundException;
 
     /**
      * Retrieves the list of subscribers to a report.
@@ -112,7 +111,7 @@ public interface UserGateway {
      * @return The list of subscribers to {@code report}.
      * @throws NotFoundException The report could not be found.
      */
-    public List<User> getSubscribersOf(Report report) throws NotFoundException;
+    List<User> getSubscribersOf(Report report) throws NotFoundException;
 
     /**
      * Retrieves the list of subscribers to a topic.
@@ -121,14 +120,14 @@ public interface UserGateway {
      * @return The list of subscribers to {@code topic}.
      * @throws NotFoundException The topic could not be found.
      */
-    public List<User> getSubscribersOf(Topic topic) throws NotFoundException;
+    List<User> getSubscribersOf(Topic topic) throws NotFoundException;
 
     /**
-     * Inserts a user into the user storage.
+     * Inserts a user into the user storage and sets its internal id, i.e. changes the given {@code user}.
      *
      * @param user The user to insert.
      */
-    public void createUser(User user);
+    void createUser(User user);
 
     /**
      * Updates a user's attributes in the user storage.
@@ -136,7 +135,7 @@ public interface UserGateway {
      * @param user The user to update.
      * @throws NotFoundException The user could not be found.
      */
-    public void updateUser(User user) throws NotFoundException;
+    void updateUser(User user) throws NotFoundException;
 
     /**
      * Deletes a user from the user storage.
@@ -144,6 +143,6 @@ public interface UserGateway {
      * @param user The user to delete.
      * @throws NotFoundException The user could not be found.
      */
-    public void deleteUser(User user) throws NotFoundException;
+    void deleteUser(User user) throws NotFoundException;
 
 }
