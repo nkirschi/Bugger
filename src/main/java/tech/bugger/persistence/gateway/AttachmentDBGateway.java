dba575ba9535cd35fc8dbc8947187b76c40ed880
Peter Mader <maderp@fim.uni-passau.de>
2021-01-03 21:49:32 +0100

    Implement Attachment Gateway Functionality

--- src/main/java/tech/bugger/persistence/gateway/AttachmentDBGateway.java
+++ src/main/java/tech/bugger/persistence/gateway/AttachmentDBGateway.java
@@ -3,8 +3,14 @@ package tech.bugger.persistence.gateway;
 import tech.bugger.global.transfer.Attachment;
 import tech.bugger.global.transfer.Post;
 import tech.bugger.global.util.Log;
+import tech.bugger.persistence.exception.NotFoundException;
+import tech.bugger.persistence.exception.StoreException;
+import tech.bugger.persistence.util.StatementParametrizer;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -12,8 +18,14 @@ import java.util.List;
  */
 public class AttachmentDBGateway implements AttachmentGateway {
 
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
     private static final Log log = Log.forClass(AttachmentDBGateway.class);
 
+    /**
+     * Database connection used by this gateway.
+     */
     private Connection conn;
 
     /**
@@ -21,41 +33,96 @@ public class AttachmentDBGateway implements AttachmentGateway {
      *
      * @param conn The database connection to use for the gateway.
      */
-    public AttachmentDBGateway(Connection conn) {
-        //this.conn = conn;
+    public AttachmentDBGateway(final Connection conn) {
+        this.conn = conn;
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void create(Attachment attachment) {
-        // TODO Auto-generated method stub
+    public void create(final Attachment attachment) {
+        try (PreparedStatement stmt = conn.prepareStatement(
+                "INSERT INTO attachment (name, content, mimetype, post)"
+                        + "VALUES (?, ?, ?, ?);",
+                PreparedStatement.RETURN_GENERATED_KEYS
+        )) {
+            PreparedStatement statement = new StatementParametrizer(stmt)
+                    .string(attachment.getName())
+                    .bytes(attachment.getContent().get())
+                    .string(attachment.getMimetype())
+                    .integer(attachment.getPost().get().getId())
+                    .toStatement();
+            statement.executeUpdate();
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                attachment.setId(generatedKeys.getInt("id"));
+            } else {
+                log.error("Error while retrieving new attachment ID.");
+                throw new StoreException("Error while retrieving new attachment ID.");
+            }
+        } catch (SQLException e) {
+            log.error("Error while creating attachment.", e);
+            throw new StoreException("Error while creating attachment.", e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void update(Attachment attachment) {
-        // TODO Auto-generated method stub
-
+    public void update(final Attachment attachment) throws NotFoundException {
+        try (PreparedStatement stmt = conn.prepareStatement(
+                "UPDATE attachment "
+                        + "SET name = ?, content = ?, mimetype = ?, post = ? "
+                        + "WHERE id = ?;"
+        )) {
+            int rowsAffected = new StatementParametrizer(stmt)
+                    .string(attachment.getName())
+                    .bytes(attachment.getContent().get())
+                    .string(attachment.getMimetype())
+                    .integer(attachment.getPost().get().getId())
+                    .integer(attachment.getId())
+                    .toStatement().executeUpdate();
+            if (rowsAffected == 0) {
+                // TODO: How do we attach the DTO to a NotFoundException?
+                log.error("Attachment to be updated could not be found.");
+                throw new NotFoundException("Attachment to be updated could not be found.");
+            }
+        } catch (SQLException e) {
+            log.error("Error while updating attachment.", e);
+            throw new StoreException("Error while updating attachment.", e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void delete(Attachment attachment) {
-        // TODO Auto-generated method stub
-
+    public void delete(final Attachment attachment) throws NotFoundException {
+        try (PreparedStatement stmt = conn.prepareStatement(
+                "DELETE FROM attachment "
+                        + "WHERE id = ?;"
+        )) {
+            int rowsAffected = new StatementParametrizer(stmt)
+                    .integer(attachment.getId())
+                    .toStatement().executeUpdate();
+            if (rowsAffected == 0) {
+                log.error("Attachment to be deleted could not be found.");
+                throw new NotFoundException("Attachment to be deleted could not be found.");
+            }
+        } catch (SQLException e) {
+            log.error("Error while deleting attachment.", e);
+            throw new StoreException("Error while deleting attachment.", e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Attachment getContentByID(int id) {
+    public Attachment getContentByID(final int id) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -64,7 +131,7 @@ public class AttachmentDBGateway implements AttachmentGateway {
      * {@inheritDoc}
      */
     @Override
-    public List<Attachment> getAttachmentsForPost(Post post) {
+    public List<Attachment> getAttachmentsForPost(final Post post) {
         // TODO Auto-generated method stub
         return null;
     }
---
+++ src/test/java/tech/bugger/persistence/gateway/AttachmentDBGatewayTest.java
@@ -0,0 +1,140 @@
+package tech.bugger.persistence.gateway;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import tech.bugger.DBExtension;
+import tech.bugger.LogExtension;
+import tech.bugger.global.transfer.Attachment;
+import tech.bugger.global.transfer.Post;
+import tech.bugger.global.util.Lazy;
+import tech.bugger.persistence.exception.NotFoundException;
+import tech.bugger.persistence.exception.StoreException;
+import tech.bugger.persistence.util.StatementParametrizer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(LogExtension.class)
+@ExtendWith(DBExtension.class)
+public class AttachmentDBGatewayTest {
+
+    private AttachmentDBGateway gateway;
+    private Connection connection;
+    private Post post;
+    private Attachment attachment;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        connection = DBExtension.getConnection();
+        gateway = new AttachmentDBGateway(connection);
+
+        post = new Post(100, "", null, null, null);
+        byte[] content = "Some random byte string".getBytes();
+        attachment = new Attachment(2, "test.txt", new Lazy<>(content), "text/plain", new Lazy<>(post));
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        connection.close();
+    }
+
+    private Attachment find(int id) throws Exception {
+        // necessary until AttachmentDBGateway#find is implemented
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM attachment WHERE id = ?;");
+        ResultSet rs = new StatementParametrizer(stmt).integer(id).toStatement().executeQuery();
+
+        if (rs.next()) {
+            return new Attachment(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    new Lazy<>(rs.getBytes("content")),
+                    rs.getString("mimetype"),
+                    null);
+        } else {
+            return null;
+        }
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        gateway.create(attachment);
+        assertEquals(attachment, find(attachment.getId()));
+    }
+
+    @Test
+    public void testCreateNoKeysGenerated() throws Exception {
+        Connection connectionSpy = spy(connection);
+        PreparedStatement stmtMock = mock(PreparedStatement.class);
+        ResultSet rsMock = mock(ResultSet.class);
+        doReturn(stmtMock).when(connectionSpy).prepareStatement(any(), anyInt());
+        when(stmtMock.getGeneratedKeys()).thenReturn(rsMock);
+        when(rsMock.next()).thenReturn(false);
+        assertThrows(StoreException.class,
+                () -> new AttachmentDBGateway(connectionSpy).create(attachment));
+    }
+
+    @Test
+    public void testCreateWhenDatabaseError() throws Exception {
+        Connection connectionSpy = spy(connection);
+        doThrow(SQLException.class).when(connectionSpy).prepareStatement(any(), anyInt());
+        Attachment attachmentMock = mock(Attachment.class);
+        assertThrows(StoreException.class,
+                () -> new AttachmentDBGateway(connectionSpy).create(attachmentMock));
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Attachment attachment = find(1);
+        attachment.setPost(new Lazy<>(post));
+        attachment.setName("a-different-file-name.txt");
+        gateway.update(attachment);
+        assertEquals(attachment, find(attachment.getId()));
+    }
+
+    @Test
+    public void testUpdateWhenNotExists() throws Exception {
+        attachment.setId(42);
+        assertThrows(NotFoundException.class, () -> gateway.update(attachment));
+    }
+
+    @Test
+    public void testUpdateWhenDatabaseError() throws Exception {
+        Connection connectionSpy = spy(connection);
+        doThrow(SQLException.class).when(connectionSpy).prepareStatement(any());
+        Attachment attachmentMock = mock(Attachment.class);
+        assertThrows(StoreException.class,
+                () -> new AttachmentDBGateway(connectionSpy).update(attachmentMock));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        Attachment attachment = find(1);
+        gateway.delete(attachment);
+        assertEquals(null, find(1));
+    }
+
+    @Test
+    public void testDeleteWhenNotExists() throws Exception {
+        attachment.setId(42);
+        assertThrows(NotFoundException.class, () -> gateway.update(attachment));
+    }
+
+    @Test
+    public void testDeleteWhenDatabaseError() throws Exception {
+        Connection connectionSpy = spy(connection);
+        doThrow(SQLException.class).when(connectionSpy).prepareStatement(any());
+        Attachment attachmentMock = mock(Attachment.class);
+        assertThrows(StoreException.class,
+                () -> new AttachmentDBGateway(connectionSpy).delete(attachmentMock));
+    }
+
+}
