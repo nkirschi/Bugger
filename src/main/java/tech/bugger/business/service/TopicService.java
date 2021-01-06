28e13f64712527facae4db69911d4835fee589fb
Ben <huberb@fim.uni-passau.de>
2021-01-06 12:15:17 +0100

    Fix CheckStyle

--- src/main/java/tech/bugger/business/service/TopicService.java
+++ src/main/java/tech/bugger/business/service/TopicService.java
@@ -7,7 +7,6 @@ import tech.bugger.global.transfer.Selection;
 import tech.bugger.global.transfer.Topic;
 import tech.bugger.global.transfer.User;
 import tech.bugger.global.util.Log;
-import tech.bugger.persistence.exception.NotFoundException;
 import tech.bugger.persistence.exception.TransactionException;
 import tech.bugger.persistence.util.Transaction;
 import tech.bugger.persistence.util.TransactionManager;
