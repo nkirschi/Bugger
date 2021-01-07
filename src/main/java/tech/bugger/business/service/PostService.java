8723c0fb51e87ce81706a650cd678aac265150e1
Ben <huberb@fim.uni-passau.de>
2021-01-07 22:03:43 +0100

    Fix CheckStyle

--- src/main/java/tech/bugger/business/service/PostService.java
+++ src/main/java/tech/bugger/business/service/PostService.java
@@ -1,32 +1,80 @@
 package tech.bugger.business.service;
 
+import tech.bugger.business.internal.ApplicationSettings;
 import tech.bugger.business.util.Feedback;
+import tech.bugger.business.util.RegistryKey;
 import tech.bugger.global.transfer.Attachment;
 import tech.bugger.global.transfer.Post;
 import tech.bugger.global.transfer.User;
 import tech.bugger.global.util.Log;
+import tech.bugger.persistence.util.TransactionManager;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
 import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * Service providing methods related to posts and attachments. A {@code Feedback} event is fired, if unexpected
  * circumstances occur.
  */
-@Dependent
+@ApplicationScoped
 public class PostService {
 
+    /**
+     * The {@link Log} instance associated with this class for logging purposes.
+     */
     private static final Log log = Log.forClass(PostService.class);
 
-    @Inject
-    NotificationService notificationService;
+    /**
+     * Notification service used for sending notifications.
+     */
+    private final NotificationService notificationService;
 
+    /**
+     * The current application settings.
+     */
+    private ApplicationSettings applicationSettings;
+
+    /**
+     * Transaction manager used for creating transactions.
+     */
+    private final TransactionManager transactionManager;
+
+    /**
+     * Feedback Event for user feedback.
+     */
+    private final Event<Feedback> feedbackEvent;
+
+    /**
+     * Resource bundle for feedback messages.
+     */
+    private final ResourceBundle messagesBundle;
+
+    /**
+     * Constructs a new post service with the given dependencies.
+     *
+     * @param notificationService The notification service to use for sending notifications.
+     * @param applicationSettings The application settings to use.
+     * @param transactionManager  The transaction manager to use for creating transactions.
+     * @param feedbackEvent       The feedback event to use for user feedback.
+     * @param messagesBundle      The resource bundle for feedback messages.
+     */
     @Inject
-    @Any
-    Event<Feedback> feedback;
+    public PostService(final NotificationService notificationService, final ApplicationSettings applicationSettings,
+                       final TransactionManager transactionManager, final Event<Feedback> feedbackEvent,
+                       final @RegistryKey("messages") ResourceBundle messagesBundle) {
+        this.notificationService = notificationService;
+        this.applicationSettings = applicationSettings;
+        this.transactionManager = transactionManager;
+        this.feedbackEvent = feedbackEvent;
+        this.messagesBundle = messagesBundle;
+    }
+
+
 
     /**
      * Updates an existing post and notifies users about the change. Notifications are handled by the {@code
@@ -52,7 +100,7 @@ public class PostService {
      *
      * @param post The post to be deleted.
      */
-    public void deletePost(Post post) {
+    public void deletePost(final Post post) {
 
     }
 
