1a8fed07d4d57208470a065ad813614e7439cea3
Ben <huberb@fim.uni-passau.de>
2021-01-13 20:43:12 +0100

    Add unsubscribe

--- src/main/java/tech/bugger/persistence/gateway/SubscriptionDBGateway.java
+++ src/main/java/tech/bugger/persistence/gateway/SubscriptionDBGateway.java
@@ -11,6 +11,7 @@ import tech.bugger.persistence.util.StatementParametrizer;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -55,6 +56,7 @@ public class SubscriptionDBGateway implements SubscriptionGateway {
             log.error("Cannot subscribe user with ID null to topic " + topic + ".");
             throw new IllegalArgumentException("User ID cannot be null.");
         }
+
         String sql = "INSERT INTO topic_subscription (subscriber, topic) VALUES (?, ?);";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             PreparedStatement statement = new StatementParametrizer(stmt)
@@ -96,6 +98,7 @@ public class SubscriptionDBGateway implements SubscriptionGateway {
             log.error("Cannot subscribe user with ID null to report " + report + ".");
             throw new IllegalArgumentException("User ID cannot be null.");
         }
+
         String sql = "INSERT INTO report_subscription (subscriber, report) VALUES (?, ?);";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             PreparedStatement statement = new StatementParametrizer(stmt)
@@ -166,27 +169,129 @@ public class SubscriptionDBGateway implements SubscriptionGateway {
      * {@inheritDoc}
      */
     @Override
-    public void unsubscribe(final Topic topic, final User subscriber) {
-        // TODO Auto-generated method stub
+    public void unsubscribe(final Topic topic, final User subscriber) throws NotFoundException {
+        if (topic == null) {
+            log.error("Cannot unsubscribe from topic null.");
+            throw new IllegalArgumentException("Topic cannot be null.");
+        } else if (topic.getId() == null) {
+            log.error("Cannot unsubscribe from topic with ID null.");
+            throw new IllegalArgumentException("Topic ID cannot be null.");
+        } else if (subscriber == null) {
+            log.error("Cannot unsubscribe user null from topic " + topic + ".");
+            throw new IllegalArgumentException("User cannot be null.");
+        } else if (subscriber.getId() == null) {
+            log.error("Cannot unsubscribe user with ID null from topic " + topic + ".");
+            throw new IllegalArgumentException("User ID cannot be null.");
+        }
 
+        String sql = "DELETE FROM topic_subscription WHERE subscriber = ? AND topic = ? RETURNING *;";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            PreparedStatement statement = new StatementParametrizer(stmt)
+                    .integer(subscriber.getId())
+                    .integer(topic.getId()).toStatement();
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                if (rs.getInt("subscriber") != subscriber.getId()
+                        || rs.getInt("topic") != topic.getId()) {
+                    throw new InternalError("Wrong subscription deleted! Please investigate! Expected subscriber: "
+                            + subscriber + ", expected topic: " + topic + ", actual subscriber ID: "
+                            + rs.getInt("subscriber") + ", actual topic ID: " + rs.getInt("topic") + ".");
+                }
+            } else {
+                log.error("Subscription of user " + subscriber + " to topic " + topic + " could not be found.");
+                throw new NotFoundException("Subscription of user " + subscriber + " to topic " + topic
+                        + " could not be found.");
+            }
+        } catch (SQLException e) {
+            log.error("Error when unsubscribing user " + subscriber + " from topic " + topic + ".", e);
+            throw new StoreException("Error when unsubscribing user " + subscriber + " from topic " + topic + ".", e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void unsubscribe(final Report report, final User subscriber) {
-        // TODO Auto-generated method stub
+    public void unsubscribe(final Report report, final User subscriber) throws NotFoundException {
+        if (report == null) {
+            log.error("Cannot unsubscribe from report null.");
+            throw new IllegalArgumentException("Report cannot be null.");
+        } else if (report.getId() == null) {
+            log.error("Cannot unsubscribe from report with ID null.");
+            throw new IllegalArgumentException("Report ID cannot be null.");
+        } else if (subscriber == null) {
+            log.error("Cannot unsubscribe user null from report " + report + ".");
+            throw new IllegalArgumentException("User cannot be null.");
+        } else if (subscriber.getId() == null) {
+            log.error("Cannot unsubscribe user with ID null from report " + report + ".");
+            throw new IllegalArgumentException("User ID cannot be null.");
+        }
 
+        String sql = "DELETE FROM report_subscription WHERE subscriber = ? AND report = ? RETURNING *;";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            PreparedStatement statement = new StatementParametrizer(stmt)
+                    .integer(subscriber.getId())
+                    .integer(report.getId()).toStatement();
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                if (rs.getInt("subscriber") != subscriber.getId()
+                        || rs.getInt("report") != report.getId()) {
+                    throw new InternalError("Wrong subscription deleted! Please investigate! Expected subscriber: "
+                            + subscriber + ", expected report: " + report + ", actual subscriber ID: "
+                            + rs.getInt("subscriber") + ", actual report ID: " + rs.getInt("report") + ".");
+                }
+            } else {
+                log.error("Subscription of user " + subscriber + " to report " + report + " could not be found.");
+                throw new NotFoundException("Subscription of user " + subscriber + " to report " + report
+                        + " could not be found.");
+            }
+        } catch (SQLException e) {
+            log.error("Error when unsubscribing user " + subscriber + " from report " + report + ".", e);
+            throw new StoreException("Error when unsubscribing user " + subscriber + " from report " + report + ".", e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void unsubscribe(final User subscribedTo, final User subscriber) {
-        // TODO Auto-generated method stub
+    public void unsubscribe(final User subscribedTo, final User subscriber) throws NotFoundException {
+        if (subscribedTo == null) {
+            log.error("Cannot unsubscribe from user null.");
+            throw new IllegalArgumentException("User to unsubscribe from cannot be null.");
+        } else if (subscribedTo.getId() == null) {
+            log.error("Cannot unsubscribe from user with ID null.");
+            throw new IllegalArgumentException("ID of user to unsubscribe from cannot be null.");
+        } else if (subscriber == null) {
+            log.error("Cannot unsubscribe user null from user " + subscribedTo + ".");
+            throw new IllegalArgumentException("Subscriber cannot be null.");
+        } else if (subscriber.getId() == null) {
+            log.error("Cannot unsubscribe user with ID null from user " + subscribedTo + ".");
+            throw new IllegalArgumentException("Subscriber ID cannot be null.");
+        }
 
+        String sql = "DELETE FROM user_subscription WHERE subscriber = ? AND subscribee = ? RETURNING *;";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            PreparedStatement statement = new StatementParametrizer(stmt)
+                    .integer(subscriber.getId())
+                    .integer(subscribedTo.getId()).toStatement();
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                if (rs.getInt("subscriber") != subscriber.getId()
+                        || rs.getInt("subscribee") != subscribedTo.getId()) {
+                    throw new InternalError("Wrong subscription deleted! Please investigate! Expected subscriber: "
+                            + subscriber + ", expected subscribee: " + subscribedTo + ", actual subscriber ID: "
+                            + rs.getInt("subscriber") + ", actual subscribee ID: " + rs.getInt("subscribee") + ".");
+                }
+            } else {
+                log.error("Subscription of user " + subscriber + " to user " + subscribedTo + " could not be found.");
+                throw new NotFoundException("Subscription of user " + subscriber + " to user " + subscribedTo
+                        + " could not be found.");
+            }
+        } catch (SQLException e) {
+            log.error("Error when unsubscribing user " + subscriber + " from report " + report + ".", e);
+            throw new StoreException("Error when unsubscribing user " + subscriber + " from report " + report + ".", e);
+        }
     }
 
     /**
