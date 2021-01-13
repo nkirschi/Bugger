5b3872c9e52b678a4dd45207253ca5bce88b65ae
Ben <huberb@fim.uni-passau.de>
2021-01-13 19:20:19 +0100

    Add subscribe to topic

--- src/main/java/tech/bugger/persistence/gateway/SubscriptionDBGateway.java
+++ src/main/java/tech/bugger/persistence/gateway/SubscriptionDBGateway.java
@@ -5,8 +5,12 @@ import tech.bugger.global.transfer.Topic;
 import tech.bugger.global.transfer.User;
 import tech.bugger.global.util.Log;
 import tech.bugger.persistence.exception.NotFoundException;
+import tech.bugger.persistence.exception.StoreException;
+import tech.bugger.persistence.util.StatementParametrizer;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Subscription gateway that gives access to subscription relationships stored in a database.
@@ -36,8 +40,34 @@ public class SubscriptionDBGateway implements SubscriptionGateway {
      * {@inheritDoc}
      */
     @Override
-    public void subscribe(final Topic topic, final User subscriber) {
-        // TODO Auto-generated method stub
+    public void subscribe(final Topic topic, final User subscriber) throws NotFoundException {
+        if (topic == null) {
+            log.error("Cannot subscribe to topic null.");
+            throw new IllegalArgumentException("Topic cannot be null.");
+        } else if (topic.getId() == null) {
+            log.error("Cannot subscribe to topic with ID null.");
+            throw new IllegalArgumentException("Topic ID cannot be null.");
+        } else if (subscriber == null) {
+            log.error("Cannot subscribe user null to topic " + topic + ".");
+            throw new IllegalArgumentException("User cannot be null.");
+        } else if (subscriber.getId() == null) {
+            log.error("Cannot subscribe user with ID null to topic " + topic + ".");
+            throw new IllegalArgumentException("User ID cannot be null.");
+        }
+        String sql = "INSERT INTO topic_subscription (subscriber, topic) VALUES (?, ?);";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            PreparedStatement statement = new StatementParametrizer(stmt)
+                    .integer(subscriber.getId())
+                    .integer(topic.getId()).toStatement();
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                log.error("Could not find topic " + topic + " or user " + subscriber + ".");
+                throw new NotFoundException("Could not find topic " + topic + " or user " + subscriber + ".");
+            }
+        } catch (SQLException e) {
+            log.error("Error when subscribing user " + subscriber + " to topic " + topic + ".", e);
+            throw new StoreException("Error when subscribing user " + subscriber + " to topic " + topic + ".", e);
+        }
     }
 
     /**
