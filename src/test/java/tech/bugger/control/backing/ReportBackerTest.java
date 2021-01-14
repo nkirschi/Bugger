cee9ba6d693654b2f4224345a41ad9570022865f
caspar07 <casparil@fim.uni-passau.de>
2021-01-14 19:32:19 +0100

    Fix tests

--- src/test/java/tech/bugger/control/backing/ReportBackerTest.java
+++ src/test/java/tech/bugger/control/backing/ReportBackerTest.java
@@ -11,20 +11,37 @@ import tech.bugger.business.internal.UserSession;
 import tech.bugger.business.service.PostService;
 import tech.bugger.business.service.ReportService;
 import tech.bugger.business.service.TopicService;
+import tech.bugger.business.util.Paginator;
 import tech.bugger.global.transfer.Authorship;
+import tech.bugger.global.transfer.Configuration;
 import tech.bugger.global.transfer.Language;
 import tech.bugger.global.transfer.Report;
 import tech.bugger.global.transfer.User;
 import tech.bugger.global.util.Lazy;
 
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import java.lang.reflect.Field;
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(LogExtension.class)
@@ -49,6 +66,12 @@ public class ReportBackerTest {
 
     @Mock
     private ApplicationSettings settings;
+
+    @Mock
+    private ExternalContext ectx;
+
+    @Mock
+    private Configuration configuration;
 
     private User user;
     private Report report;
@@ -144,86 +167,6 @@ public class ReportBackerTest {
         assertFalse(reportBacker.isBanned());
     }
 
-}
-
-package tech.bugger.control.backing;
-
-import java.lang.reflect.Field;
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import tech.bugger.LogExtension;
-import tech.bugger.business.internal.ApplicationSettings;
-import tech.bugger.business.internal.UserSession;
-import tech.bugger.business.service.PostService;
-import tech.bugger.business.service.ReportService;
-import tech.bugger.business.service.TopicService;
-import tech.bugger.business.util.Paginator;
-import tech.bugger.global.transfer.Authorship;
-import tech.bugger.global.transfer.Configuration;
-import tech.bugger.global.transfer.Report;
-import tech.bugger.global.transfer.User;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
-@ExtendWith(LogExtension.class)
-@ExtendWith(MockitoExtension.class)
-public class ReportBackerTest {
-
-    private ReportBacker reportBacker;
-
-    @Mock
-    private ApplicationSettings applicationSettings;
-
-    @Mock
-    private TopicService topicService;
-
-    @Mock
-    private ReportService reportService;
-
-    @Mock
-    private PostService postService;
-
-    @Mock
-    private UserSession session;
-
-    @Mock
-    private FacesContext fctx;
-
-    @Mock
-    private ExternalContext ectx;
-
-    @Mock
-    private Configuration configuration;
-
-    @Mock
-    private User user;
-
-    private Report testReport;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        lenient().doReturn(user).when(session).getUser();
-        lenient().doReturn(configuration).when(applicationSettings).getConfiguration();
-        lenient().doReturn(ectx).when(fctx).getExternalContext();
-        reportBacker = new ReportBacker(applicationSettings, reportService, postService, topicService,
-                session, fctx);
-
-        testReport = new Report(100, "Some title", Report.Type.BUG, Report.Severity.RELEVANT, "", mock(Authorship.class),
-                mock(ZonedDateTime.class), null, null, 1);
-    }
-
     @Test
     public void testMarkDuplicateNoneSelected() {
         reportBacker.setDuplicateOfID(null);
@@ -235,17 +178,14 @@ public class ReportBackerTest {
     @Test
     public void testMarkDuplicateInvalidIsSelected() {
         reportBacker.setDuplicateOfID(42);
-        doReturn(true).when(reportService).isPrivileged(any(), any());
-        doReturn(false).when(reportService).markDuplicate(any(), eq(42));
         reportBacker.markDuplicate();
         verify(reportService, never()).close(any());
     }
 
     @Test
     public void testMarkDuplicateValidIsSelectedAndUnprivileged() {
-        reportBacker.setReport(testReport);
+        reportBacker.setReport(report);
         reportBacker.setDuplicateOfID(100);
-        doReturn(false).when(reportService).isPrivileged(any(), any());
         reportBacker.markDuplicate();
         verify(reportService, never()).markDuplicate(any(), anyInt());
         verify(reportService, never()).close(any());
@@ -253,9 +193,10 @@ public class ReportBackerTest {
 
     @Test
     public void testMarkDuplicateValidIsSelectedAndPrivileged() throws Exception {
-        reportBacker.setReport(testReport);
+        reportBacker.setReport(report);
         reportBacker.setDuplicateOfID(100);
-        doReturn(true).when(reportService).isPrivileged(any(), any());
+        user.setAdministrator(true);
+        when(session.getUser()).thenReturn(user);
         doReturn(true).when(reportService).markDuplicate(any(), eq(100));
         Paginator<?> duplicates = mock(Paginator.class);
 
@@ -271,7 +212,6 @@ public class ReportBackerTest {
     @Test
     public void testUnmarkDuplicateUnprivileged() {
         reportBacker.setDuplicateOfID(100);
-        doReturn(false).when(reportService).isPrivileged(any(), any());
         reportBacker.unmarkDuplicate();
         assertAll(() -> assertNotNull(reportBacker.getDuplicateOfID()),
                 () -> verify(reportService, never()).unmarkDuplicate(any()));
@@ -280,8 +220,6 @@ public class ReportBackerTest {
     @Test
     public void testUnmarkDuplicateError() {
         reportBacker.setDuplicateOfID(100);
-        doReturn(true).when(reportService).isPrivileged(any(), any());
-        doReturn(false).when(reportService).unmarkDuplicate(any());
         reportBacker.unmarkDuplicate();
         assertNotNull(reportBacker.getDuplicateOfID());
     }
@@ -289,7 +227,9 @@ public class ReportBackerTest {
     @Test
     public void testUnmarkDuplicateSuccess() {
         reportBacker.setDuplicateOfID(100);
-        doReturn(true).when(reportService).isPrivileged(any(), any());
+        reportBacker.setReport(report);
+        user.setAdministrator(true);
+        when(session.getUser()).thenReturn(user);
         doReturn(true).when(reportService).unmarkDuplicate(any());
         reportBacker.unmarkDuplicate();
         assertNull(reportBacker.getDuplicateOfID());
@@ -303,8 +243,10 @@ public class ReportBackerTest {
         doReturn(duplicates.size()).when(reportService).getNumberOfDuplicates(any());
         doReturn(duplicates).when(reportService).getDuplicatesFor(any(), any());
         doReturn(true).when(configuration).isGuestReading();
+        doReturn(ectx).when(fctx).getExternalContext();
         doReturn(Map.of("id", "100")).when(ectx).getRequestParameterMap();
-        doReturn(testReport).when(reportService).getReportByID(100);
+        doReturn(configuration).when(settings).getConfiguration();
+        doReturn(report).when(reportService).getReportByID(100);
 
         reportBacker.init();
 
