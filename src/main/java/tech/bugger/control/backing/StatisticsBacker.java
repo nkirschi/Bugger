0ca3220fb8714fed05331845a9804a0311ba9693
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-01-14 22:57:11 +0100

    Remove sout

--- src/main/java/tech/bugger/control/backing/StatisticsBacker.java
+++ src/main/java/tech/bugger/control/backing/StatisticsBacker.java
@@ -170,7 +170,6 @@ public class StatisticsBacker implements Serializable {
      * @return {@code null} in order to reload the page.
      */
     public String applyFilters() {
-        System.out.println("TOPIC: " + reportCriteria.getTopic());
         ResourceBundle messagesBundle = registry.getBundle("messages", userSession);
         feedbackEvent.fire(new Feedback(messagesBundle.getString("filters_applied"), Feedback.Type.INFO));
         return null;
