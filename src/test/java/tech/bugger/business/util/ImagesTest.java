1c7bc96848b74685a219ad2b90aff2134e23e6bf
Nico Mexis <nico.mexis@kabelmail.de>
2021-01-18 14:04:32 +0100

    ADD ALL THE TESTS
    Actually, like last time, not all tests, just a nice reference to the "All the things" meme again

--- src/test/java/tech/bugger/business/util/ImagesTest.java
+++ src/test/java/tech/bugger/business/util/ImagesTest.java
@@ -1,12 +1,16 @@
 package tech.bugger.business.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import javax.imageio.ImageIO;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import tech.bugger.business.exception.CorruptImageException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ImagesTest {
 
@@ -30,10 +34,18 @@ public class ImagesTest {
     }
 
     @Test
-    public void testGenerateThumbnailCorruptImage() throws CorruptImageException {
+    public void testGenerateThumbnailCorruptImage() {
         assertThrows(CorruptImageException.class,
                 () -> Images.generateThumbnail(new byte[10])
         );
+    }
+
+    @Test
+    public void testGenerateThumbnailIOException() {
+        try (MockedStatic<ImageIO> imageIOMock = mockStatic(ImageIO.class)) {
+            imageIOMock.when(() -> ImageIO.read(any(ByteArrayInputStream.class))).thenThrow(IOException.class);
+            assertThrows(CorruptImageException.class, () -> Images.generateThumbnail(new byte[10]));
+        }
     }
 
 }
--- src/test/java/tech/bugger/control/backing/FooterBackerTest.java
+++ src/test/java/tech/bugger/control/backing/FooterBackerTest.java
@@ -1,4 +1,65 @@
 package tech.bugger.control.backing;
 
+import java.util.Locale;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.bugger.LogExtension;
+import tech.bugger.ResourceBundleMocker;
+import tech.bugger.business.internal.UserSession;
+import tech.bugger.business.util.MarkdownHandler;
+import tech.bugger.business.util.Registry;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(LogExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class FooterBackerTest {
+
+    private FooterBacker backer;
+
+    @Mock
+    private UserSession session;
+
+    @Mock
+    private Registry registry;
+
+    @BeforeEach
+    public void setUp() {
+        doReturn(ResourceBundleMocker.mock("help")).when(registry)
+                .getBundle(anyString(), any());
+        doReturn(Locale.GERMAN).when(session).getLocale();
+        backer = new FooterBacker(session, registry);
+    }
+
+    @Test
+    public void testToggleHelpToFalse() {
+        backer.setHelpDisplayed(true);
+        String site = backer.toggleHelp();
+        assertAll(() -> assertNull(site),
+                () -> assertFalse(backer.isHelpDisplayed()));
+    }
+
+    @Test
+    public void testToggleHelpToTrue() {
+        backer.setHelpDisplayed(false);
+        String site = backer.toggleHelp();
+        assertAll(() -> assertNull(site),
+                () -> assertTrue(backer.isHelpDisplayed()));
+    }
+
+    @Test
+    public void testGetHelp() {
+        try (MockedStatic<MarkdownHandler> handlerMock = mockStatic(MarkdownHandler.class)) {
+            String html = "nananananana batman";
+            handlerMock.when(() -> MarkdownHandler.toHtml(any())).thenReturn(html);
+            assertEquals(html, backer.getHelp("helpKey"));
+            handlerMock.verify(() -> MarkdownHandler.toHtml(eq("help\n\nhelp")));
+        }
+    }
+
 }
