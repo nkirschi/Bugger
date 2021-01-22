34a41d5771364b1019747b3d2ec531d70fff2860
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-01-22 10:08:49 +0100

    Fix Constructor Syntax

--- src/test/java/tech/bugger/control/backing/HeaderBackerTest.java
+++ src/test/java/tech/bugger/control/backing/HeaderBackerTest.java
@@ -1,17 +1,5 @@
 package tech.bugger.control.backing;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-import javax.enterprise.event.Event;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-
 import com.ocpsoft.pretty.PrettyContext;
 import com.ocpsoft.pretty.faces.config.mapping.UrlMapping;
 import com.sun.faces.context.RequestParameterMap;
@@ -30,17 +18,26 @@ import tech.bugger.business.service.SearchService;
 import tech.bugger.business.util.Feedback;
 import tech.bugger.global.transfer.User;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import javax.enterprise.event.Event;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(LogExtension.class)
 @ExtendWith(MockitoExtension.class)
@@ -98,7 +95,7 @@ public class HeaderBackerTest {
         lenient().doReturn(request).when(ectx).getRequest();
         config = mockStatic(PrettyContext.class);
         config.when(PrettyContext::getCurrentInstance).thenReturn(prettyContext);
-        headerBacker = new HeaderBacker(settings, searchService, session, fctx, ectx, feedback, messages);
+        headerBacker = new HeaderBacker(searchService, session, fctx, ectx, feedback, messages);
     }
 
     @AfterEach
