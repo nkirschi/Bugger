138b292ed8c0c32d677c936be2bceff318aaf67f
Ben <huberb@fim.uni-passau.de>
2021-02-03 00:24:09 +0100

    Add some more tests for ReportBacker

--- src/test/java/tech/bugger/control/backing/ReportBackerTest.java
+++ src/test/java/tech/bugger/control/backing/ReportBackerTest.java
@@ -11,6 +11,7 @@ import tech.bugger.business.internal.UserSession;
 import tech.bugger.business.service.PostService;
 import tech.bugger.business.service.ReportService;
 import tech.bugger.business.service.TopicService;
+import tech.bugger.business.util.Paginator;
 import tech.bugger.control.exception.Error404Exception;
 import tech.bugger.global.transfer.Authorship;
 import tech.bugger.global.transfer.Configuration;
@@ -19,6 +20,7 @@ import tech.bugger.global.transfer.Report;
 import tech.bugger.global.transfer.User;
 
 import javax.faces.context.ExternalContext;
+import java.lang.reflect.Field;
 import java.time.OffsetDateTime;
 import java.util.Locale;
 
@@ -218,8 +220,188 @@ public class ReportBackerTest {
     }
 
     @Test
-    public void testMarkDuplicate() {
+    public void testMarkDuplicate() throws Exception {
+        report.setDuplicateOf(42);
         reportBacker.setReport(report);
+        Field f = ReportBacker.class.getDeclaredField("privileged");
+        f.setAccessible(true);
+        f.set(reportBacker, true);
+        f = ReportBacker.class.getDeclaredField("duplicates");
+        f.setAccessible(true);
+        f.set(reportBacker, mock(Paginator.class));
+        doReturn(true).when(reportService).markDuplicate(report, 42);
+        assertDoesNotThrow(() -> reportBacker.markDuplicate());
+        verify(reportService).markDuplicate(report, 42);
+        verify(reportService).close(report);
+        assertNull(reportBacker.getCurrentDialog());
+    }
+
+    @Test
+    public void testMarkDuplicateWhenUserIsNotPrivileged() {
+        assertDoesNotThrow(() -> reportBacker.markDuplicate());
+        verify(reportService, never()).close(any());
+        verify(reportService, never()).markDuplicate(any(), anyInt());
+    }
+
+    @Test
+    public void testMarkDuplicateWhenMarkingFails() throws Exception {
+        report.setDuplicateOf(42);
+        reportBacker.setReport(report);
+        Field f = ReportBacker.class.getDeclaredField("privileged");
+        f.setAccessible(true);
+        f.set(reportBacker, true);
+        assertDoesNotThrow(() -> reportBacker.markDuplicate());
+        verify(reportService).markDuplicate(report, 42);
+        verify(reportService, never()).close(any());
+    }
+
+    @Test
+    public void testUnmarkDuplicateWhenUserIsNotPrivileged() {
+        assertDoesNotThrow(() -> reportBacker.unmarkDuplicate());
+        verify(reportService, never()).unmarkDuplicate(any());
+    }
+
+    @Test
+    public void testUnmarkDuplicate() throws Exception {
+        reportBacker.setReport(report);
+        Field f = ReportBacker.class.getDeclaredField("privileged");
+        f.setAccessible(true);
+        f.set(reportBacker, true);
+        assertDoesNotThrow(() -> reportBacker.unmarkDuplicate());
+        verify(reportService).unmarkDuplicate(report);
+    }
+
+    @Test
+    public void testApplyOverwriteRelevanceWhenUserIsAnon() {
+        assertThrows(Error404Exception.class, () -> reportBacker.applyOverwriteRelevance());
+    }
+
+    @Test
+    public void testApplyOverwriteRelevanceWhenUserIsNoAdmin() {
+        doReturn(user).when(session).getUser();
+        assertThrows(Error404Exception.class, () -> reportBacker.applyOverwriteRelevance());
+    }
+
+    @Test
+    public void testApplyOverwriteRelevance() {
+        user.setAdministrator(true);
+        doReturn(user).when(session).getUser();
+        reportBacker.setReport(report);
+        reportBacker.setOverwriteRelevanceValue(42);
+        doReturn(report).when(reportService).getReportByID(anyInt());
+        assertNull(reportBacker.applyOverwriteRelevance());
+        verify(reportService).overwriteRelevance(report, 42);
+    }
+
+    @Test
+    public void testDeletePost() {
+        Post post = new Post(42, "a", 100, null, null);
+        reportBacker.setReport(report);
+        reportBacker.setPostToBeDeleted(post);
+        assertDoesNotThrow(() -> reportBacker.deletePost());
+        verify(postService).deletePost(post, report);
+        assertNull(reportBacker.getCurrentDialog());
+    }
+
+    @Test
+    public void testIsAllowedToPostWhenUserIsAnon() {
+        assertFalse(reportBacker.isAllowedToPost());
+    }
+
+    @Test
+    public void testIsAllowedToPostWhenUserIsBanned() throws Exception {
+        doReturn(user).when(session).getUser();
+        Field f = ReportBacker.class.getDeclaredField("banned");
+        f.setAccessible(true);
+        f.set(reportBacker, true);
+        assertFalse(reportBacker.isAllowedToPost());
+    }
+
+    @Test
+    public void testIsAllowedToPostWhenReportIsOpen() {
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(null);
+        reportBacker.setReport(report);
+        assertTrue(reportBacker.isAllowedToPost());
+    }
+
+    @Test
+    public void testIsAllowedToPostWhenClosedReportPostingEnabled() {
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(OffsetDateTime.now());
+        reportBacker.setReport(report);
+        doReturn(configuration).when(settings).getConfiguration();
+        doReturn(true).when(configuration).isClosedReportPosting();
+        assertTrue(reportBacker.isAllowedToPost());
+    }
+
+    @Test
+    public void testIsAllowedToPostWhenClosedReportPostingDisabled() {
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(OffsetDateTime.now());
+        reportBacker.setReport(report);
+        doReturn(configuration).when(settings).getConfiguration();
+        assertFalse(reportBacker.isAllowedToPost());
+    }
+
+    @Test
+    public void testPrivilegedForPostWhenUserIsAnon() {
+        Post post = new Post(42, "a", 100, null, null);
+        assertFalse(reportBacker.privilegedForPost(post));
+    }
+
+    @Test
+    public void testPrivilegedForPostWhenClosedReportPostingDisabled() {
+        Post post = new Post(42, "a", 100, null, null);
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(OffsetDateTime.now());
+        reportBacker.setReport(report);
+        doReturn(configuration).when(settings).getConfiguration();
+        assertFalse(reportBacker.privilegedForPost(post));
+    }
+
+    @Test
+    public void testPrivilegedForPostWhenUserIsAdmin() {
+        Post post = new Post(42, "a", 100, null, null);
+        user.setAdministrator(true);
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(null);
+        reportBacker.setReport(report);
+        assertTrue(reportBacker.privilegedForPost(post));
+    }
+
+    @Test
+    public void testPrivilegedForPostWhenUserIsMod() throws Exception {
+        Post post = new Post(42, "a", 100, null, null);
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(null);
+        reportBacker.setReport(report);
+        Field f = ReportBacker.class.getDeclaredField("moderator");
+        f.setAccessible(true);
+        f.set(reportBacker, true);
+        assertTrue(reportBacker.privilegedForPost(post));
+    }
+
+    @Test
+    public void testPrivilegedForPostWhenUserIsAuthor() {
+        Post post = new Post(42, "a", 100, null, null);
+        Authorship authorship = new Authorship(user, null, null, null);
+        post.setAuthorship(authorship);
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(null);
+        reportBacker.setReport(report);
+        assertTrue(reportBacker.privilegedForPost(post));
+    }
+
+    @Test
+    public void testPrivilegedForPostWhenNope() {
+        Post post = new Post(42, "a", 100, null, null);
+        Authorship authorship = new Authorship(null, null, null, null);
+        post.setAuthorship(authorship);
+        doReturn(user).when(session).getUser();
+        report.setClosingDate(null);
+        reportBacker.setReport(report);
+        assertFalse(reportBacker.privilegedForPost(post));
     }
 
 }
