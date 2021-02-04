38851229af87ccdfdd113afee1291e5e63ad6149
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-02-04 19:03:44 +0100

    Perform Nasty Modifications

--- src/test/java/selenium/ModeratorTest.java
+++ src/test/java/selenium/ModeratorTest.java
@@ -12,12 +12,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static selenium.Constants.*;
 
 @ExtendWith(SeleniumExtension.class)
@@ -40,12 +39,11 @@ public class ModeratorTest {
     }
 
     @Test
-    public void T220_notifications() {
+    public void T220_discover_notifications() {
         driver.get(baseURL);
 
         // Log in as moderator.
         driver.findElement(By.id("l-login")).click();
-        driver.findElement(By.id("f-login:it-username")).click();
         driver.findElement(By.id("f-login:it-username")).sendKeys(ALF_USERNAME);
         driver.findElement(By.id("f-login:it-password")).sendKeys(ALF_PASSWORD);
         driver.findElement(By.id("f-login:cb-login")).click();
@@ -62,21 +60,21 @@ public class ModeratorTest {
     }
 
     @Test
-    public void T230_deletePost() {
-        driver.findElement(By.cssSelector("#p-notifications input[value=\"" + NEW_POST_NOTIFICATION_BUTTON + "\"]"))
-                .click();
+    public void T230_delete_post() {
+        driver.findElement(By.cssSelector("[id*=cb-notification-button][value='" + NEW_POST_NOTIFICATION_BUTTON + "']"))
+              .click();
+        // TODO maybe select by n-th id of page strategy rather?
         driver.findElement(By.cssSelector("table .mb-3:nth-child(2) input[type=\"submit\"]")).click();
         driver.findElement(By.id("f-delete-post:cb-delete-post")).click();
 
         // Only one post should be left.
-        assertEquals(EXPECTED_POST_NUM, driver.findElements(By.cssSelector("td > .mb-3")).size());
+        assertEquals(EXPECTED_POST_NUM, driver.findElements(By.cssSelector("[id^=post]")).size());
     }
 
     @Test
-    public void T240_overwriteRelevance() {
-        WebElement overwriteInput = driver.findElement(By.id("f-vote:i-overwrite-relevance-value"));
-        overwriteInput.clear();
-        overwriteInput.sendKeys(String.valueOf(OVERWRITING_RELEVANCE));
+    public void T240_overwrite_relevance() {
+        driver.findElement(By.id("f-vote:i-overwrite-relevance-value")).clear();
+        driver.findElement(By.id("f-vote:i-overwrite-relevance-value")).sendKeys(String.valueOf(OVERWRITING_RELEVANCE));
         driver.findElement(By.id("f-vote:cb-overwrite-relevance")).click();
 
         assertEquals(String.valueOf(OVERWRITING_RELEVANCE), driver.findElement(By.id("ot-relevance")).getText());
@@ -89,14 +87,14 @@ public class ModeratorTest {
     }
 
     @Test
-    public void T260_undoOverwrite() {
+    public void T260_undo_overwrite() {
         driver.findElement(By.id("f-vote:i-overwrite-relevance-value")).clear();
         driver.findElement(By.name("f-vote:cb-overwrite-relevance")).click();
         assertEquals(String.valueOf(CALCULATED_RELEVANCE), driver.findElement(By.id("ot-relevance")).getText());
     }
 
     @Test
-    public void T270_reportSearchSuggestions() {
+    public void T270_search_report_suggestions() {
         // Search for reports and check for expected suggestions.
         driver.findElement(By.id("f-search-header:it-search")).sendKeys(REPORT_SEARCH_QUERY);
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
@@ -104,18 +102,18 @@ public class ModeratorTest {
 
         List<String> suggestions = getSearchSuggestions();
         assertTrue(suggestions.size() == 2
-                && suggestions.contains(REPORT_NO_TRANSLATION)
-                && suggestions.contains(REPORT_NO_NAME));
+                           && suggestions.contains(REPORT_NO_TRANSLATION)
+                           && suggestions.contains(REPORT_NO_NAME));
     }
 
     @Test
-    public void T280_reportSearch() {
+    public void T280_search_report() {
         // Search and save search results.
         driver.findElement(By.id("f-search-header:cb-search")).click();
         List<String> resultTitles = getSearchResultTitles();
         originalID = driver.findElement(By.linkText(REPORT_NO_TRANSLATION))
-                .findElement(By.xpath("./../../td[1]/a[1]"))
-                .getText().substring(1);
+                           .findElement(By.xpath("./../../td[1]/a[1]")) // TODO does this rly need to be this ugly? :/
+                           .getText().substring(1);
 
         // Search again with additional filters.
         driver.findElement(By.id("f-search:s-show-hint-reports")).click();
@@ -125,48 +123,45 @@ public class ModeratorTest {
 
         // Check if search results are what we expected.
         assertTrue(resultTitles.size() == 2
-                && resultTitles.contains(REPORT_NO_TRANSLATION)
-                && resultTitles.contains(REPORT_NO_NAME));
-        assertEquals(Arrays.asList(REPORT_NO_NAME), resultTitlesFiltered);
+                           && resultTitles.contains(REPORT_NO_TRANSLATION)
+                           && resultTitles.contains(REPORT_NO_NAME));
+        assertEquals(Collections.singletonList(REPORT_NO_NAME), resultTitlesFiltered);
     }
 
     @Test
-    public void T290_markDuplicate() {
+    public void T290_mark_duplicate() {
         driver.findElement(By.linkText(REPORT_NO_NAME)).click();
         driver.findElement(By.name("f-report:cb-mark-duplicate")).click();
-        WebElement originalIDInput = driver.findElement(By.name("f-duplicate:it-duplicate"));
-        originalIDInput.clear();
-        originalIDInput.sendKeys(originalID);
+
+        driver.findElement(By.name("f-duplicate:it-duplicate")).clear();
+        driver.findElement(By.name("f-duplicate:it-duplicate")).sendKeys(originalID);
         driver.findElement(By.name("f-duplicate:cb-duplicate")).click();
 
-        assertEquals(driver.findElement(By.id("ot-status1")).getText(), CLOSED_AT);
-        assertEquals(driver.findElement(By.id("l-duplicate")).getText(), "#" + originalID);
+        assertAll(
+                () -> assertEquals(CLOSED_AT, driver.findElement(By.id("ot-status1")).getText()),
+                () -> assertEquals(driver.findElement(By.id("l-duplicate")).getText(), "#" + originalID)
+        );
     }
 
     @Test
-    public void T300_topicSearchSuggestions() {
+    public void T300_search_topic_suggestions() {
         driver.findElement(By.id("f-search-header:it-search")).sendKeys(TOPIC_GUI);
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
                 "#f-search-header\\:p-search-suggestions .search")));
-        assertEquals(Arrays.asList(TOPIC_GUI), getSearchSuggestions());
+        assertEquals(Collections.singletonList(TOPIC_GUI), getSearchSuggestions());
 
         driver.findElement(By.cssSelector("#f-search-header\\:p-search-suggestions .search")).click();
-        driver.findElement(By.cssSelector("#p-tab-topic-content tbody td:first-child a")).click();
-        assertTrue(driver.findElement(By.id("title")).getText().endsWith(TOPIC_GUI));
+        driver.findElement(By.cssSelector("#p-tab-topic-content tbody td:first-child a")).click(); // TODO less ugly?
+        assertTrue(driver.getTitle().contains(TOPIC_GUI));
     }
 
     @Test
-    public void T310_banUser() {
+    public void T310_ban_user() {
         driver.findElement(By.id("f-banned-status:cb-image-ban")).click();
         driver.findElement(By.id("f-ban:it-username-ban")).sendKeys(BEA_USERNAME);
         driver.findElement(By.id("f-ban:cb-ban")).click();
 
-        List<String> bannedUsers = driver
-                .findElements(By.cssSelector("#p-banned tbody a"))
-                .stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
-        assertEquals(Arrays.asList(USERNAME_PREFIX + BEA_USERNAME), bannedUsers);
+        assertEquals(Collections.singletonList(USERNAME_PREFIX + BEA_USERNAME), getBannedUsers());
     }
 
     private List<String> getSearchSuggestions() {
@@ -180,6 +175,14 @@ public class ModeratorTest {
     private List<String> getSearchResultTitles() {
         return driver
                 .findElements(By.cssSelector("#p-tab-report-content td:nth-child(2) a"))
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getBannedUsers() {
+        return driver
+                .findElements(By.cssSelector("#p-banned tbody a"))
                 .stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
