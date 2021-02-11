be52016c64c65e9fad80875eff175b9cbbe02155
Nikolas Kirschstein <kirschstei@fim.uni-passau.de>
2021-02-11 17:42:51 +0100

    Remove Verbose Debug Log

--- src/main/java/tech/bugger/business/util/ReferencePostProcessor.java
+++ src/main/java/tech/bugger/business/util/ReferencePostProcessor.java
@@ -2,17 +2,11 @@ package tech.bugger.business.util;
 
 import org.commonmark.node.Node;
 import org.commonmark.parser.PostProcessor;
-import tech.bugger.global.util.Log;
 
 /**
  * {@link PostProcessor} that parses Markdown references.
  */
 public class ReferencePostProcessor implements PostProcessor {
-
-    /**
-     * The {@link Log} instance associated with this class for logging purposes.
-     */
-    private static final Log log = Log.forClass(ReferencePostProcessor.class);
 
     /**
      * The {@link ReferenceVisitor} to use when parsing.
@@ -24,7 +18,6 @@ public class ReferencePostProcessor implements PostProcessor {
      */
     @Override
     public Node process(final Node node) {
-        log.debug("Parsing node " + node + " with reference support.");
         node.accept(visitor);
         return node;
     }
